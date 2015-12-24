e8f72aa677d6f3e826ae27dc7b17411805d55377
Mark Johnson <markj@matzsoft.com>
2015-12-24 09:56:02 -0800

    Prepare to solve part 2.
    Basically make the solver into a subroutine so that everything can be
    reset and then call it again for part 2.

--- day22.pl
+++ day22.pl
@@ -26,18 +26,23 @@ use 5.010;
 use Storable qw( dclone );
 use List::Util qw( max min );
 
+use constant DEBUG        => 0;
+use constant REPORT_LIMIT => 300;
+use constant INFINITY     => 2000000000;
 
-my @spells = (
+
+my $report_count = 0;
+my @spells       = (
     { name => 'Magic Missle', cost => 53,  damage   => 4 },
     { name => 'Drain',        cost => 73,  damage   => 2, heal => 2 },
     { name => 'Shield',       cost => 113, armor    => 7, duration => 6 },
     { name => 'Poison',       cost => 173, damage   => 3, duration => 6 },
     { name => 'Recharge',     cost => 229, duration => 5, mana => 101 },
 );
-my %spells = map { $_->{name} => $_ } @spells;
-my @timers      = map { $_->{name} } grep { exists $_->{duration} } @spells;
-my $boss_damage = 9;
-my %state       = (
+my %spells        = map { $_->{name} => $_ } @spells;
+my @timers        = map { $_->{name} } grep { exists $_->{duration} } @spells;
+my $boss_damage   = 9;
+my %initial_state = (
     hp      => 50,
     mana    => 500,
     armor   => 0,
@@ -46,51 +51,58 @@ my %state       = (
     timers  => { map { $_ => 0 } @timers },
     spells  => [ map { $_->{name} } @spells ],
 );
-my @stack = ( dclone( \%state ) );
-my $cheapest_win = 2000000000;              # Approximate infinity.
-my ( $report_count, $report_limit ) = ( 0, 300 );
+my @stack        = ( dclone( \%initial_state ) );
+my $cheapest_win = INFINITY;
+my %state;
 
-while ( @stack ) {
-    # Work with top of stack, move to state for convenience.
-    %state = %{ dclone( $stack[-1] ) };
-    report( "Stack depth ", scalar( @stack ) );
+play( 0 );
+say $cheapest_win;
+say $report_count;
 
-    implement_effects();
-    if ( players_move() ) {
-        if ( $state{spent} < $cheapest_win ) {
-            implement_effects();
 
-            if ( $state{boss_hp} <= 0 ) {
-                report( "Player wins." );
-                $cheapest_win = $state{spent};
-                say $state{spent};
 
-            } else {
-                # Boss action here.
-                $state{hp} -= max( $boss_damage - $state{armor}, 1 );
+sub play {
+    my $dificulty = shift;
 
-                if ( $state{hp} <= 0 ) {
-                    report( "Player loses." );
+    while ( @stack ) {
+        # Work with top of stack, move to state for convenience.
+        %state = %{ dclone( $stack[-1] ) };
+        report( "Stack depth ", scalar( @stack ) );
+
+        implement_effects();
+        if ( players_move() ) {
+            if ( $state{spent} < $cheapest_win ) {
+                implement_effects();
+
+                if ( $state{boss_hp} <= 0 ) {
+                    report( "Player wins." );
+                    $cheapest_win = $state{spent};
+                    say $state{spent};
 
                 } else {
-                    # Take this path one more step down the tree.
-                    push @stack, dclone( \%state );
-                    $stack[-1]{spells} = [ map { $_->{name} } @spells ];
-                    next;
+                    # Boss action here.
+                    $state{hp} -= max( $boss_damage - $state{armor}, 1 );
+
+                    if ( $state{hp} <= 0 ) {
+                        report( "Player loses." );
+
+                    } else {
+                        # Take this path one more step down the tree.
+                        push @stack, dclone( \%state );
+                        $stack[-1]{spells} = [ map { $_->{name} } @spells ];
+                        next;
+                    }
                 }
             }
         }
-    }
 
-    # We have hit an end (win, loss, or no more moves), so try again at this
-    # level or backtrack if all spells have been tried.
-    if ( not @{ $stack[-1]{spells} } ) {
-        pop @stack;
+        # We have hit an end (win, loss, or no more moves), so try again at this
+        # level or backtrack if all spells have been tried.
+        if ( not @{ $stack[-1]{spells} } ) {
+            pop @stack;
+        }
     }
 }
-
-say $cheapest_win;
-say $report_count;
 
 
 
@@ -169,17 +181,10 @@ sub players_move {
 sub report {
     $report_count++;
     
-
-=begin  BlockComment  # BlockCommentNo_1
-
-    $report_count <= $report_limit or die "Report limit exceeded\n";
-
-    say @_;
-
-=end    BlockComment  # BlockCommentNo_1
-
-=cut
-
+    if ( DEBUG ) {
+        $report_count <= REPORT_LIMIT or die "Report limit exceeded\n";
+        say @_;
+    }
 }
 
 
