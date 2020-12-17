f2d69d2457f890ba87fa704c019698a650de86e4
Mark Johnson <markj@matzsoft.com>
2020-12-17 13:16:41 -0800

    Added part2.

--- 2020/day17.swift
+++ 2020/day17.swift
@@ -8,24 +8,28 @@
 
 import Foundation
 
-struct Coordinate: Hashable {
+struct Coordinate3: Hashable {
     let x: Int, y: Int, z: Int
 }
 
-struct Dimension {
-    let active: Set<Coordinate>
+struct Coordinate4: Hashable {
+    let x: Int, y: Int, z: Int, w: Int
+}
+
+struct Dimension3 {
+    let active: Set<Coordinate3>
     
-    init( active: [Coordinate] ) {
+    init( active: [Coordinate3] ) {
         self.active = Set( active )
     }
     
     init( input: String ) {
-        var active: [Coordinate] = []
+        var active: [Coordinate3] = []
         
         for ( yindex, row ) in input.split( separator: "\n" ).enumerated() {
             for ( xindex, value ) in row.enumerated() {
                 if value == "#" {
-                    active.append( Coordinate( x: xindex, y: yindex, z: 0 ) )
+                    active.append( Coordinate3( x: xindex, y: yindex, z: 0 ) )
                 }
             }
         }
@@ -33,19 +37,19 @@ struct Dimension {
         self.init( active: active )
     }
     
-    var cycle: Dimension {
+    var cycle: Dimension3 {
         let xmin = active.min( by: { $0.x < $1.x } )!.x - 1
         let xmax = active.max( by: { $0.x < $1.x } )!.x + 1
         let ymin = active.min( by: { $0.y < $1.y } )!.y - 1
         let ymax = active.max( by: { $0.y < $1.y } )!.y + 1
         let zmin = active.min( by: { $0.z < $1.z } )!.z - 1
         let zmax = active.max( by: { $0.z < $1.z } )!.z + 1
-        var next: [Coordinate] = []
+        var next: [Coordinate3] = []
         
         for x in xmin ... xmax {
             for y in ymin ... ymax {
                 for z in zmin ... zmax {
-                    let current = Coordinate( x: x, y: y, z: z )
+                    let current = Coordinate3( x: x, y: y, z: z )
                     let neighbors = countNeighbors( coordinate: current )
                     
                     if active.contains( current ) {
@@ -61,15 +65,77 @@ struct Dimension {
             }
         }
         
-        return Dimension( active: next )
+        return Dimension3( active: next )
     }
     
-    func countNeighbors( coordinate: Coordinate ) -> Int {
+    func countNeighbors( coordinate: Coordinate3 ) -> Int {
         var sum = 0
         for x in coordinate.x - 1 ... coordinate.x + 1 {
             for y in coordinate.y - 1 ... coordinate.y + 1 {
                 for z in coordinate.z - 1 ... coordinate.z + 1 {
-                    sum += active.contains( Coordinate( x: x, y: y, z: z ) ) ? 1 : 0
+                    sum += active.contains( Coordinate3( x: x, y: y, z: z ) ) ? 1 : 0
+                }
+            }
+        }
+        return sum
+    }
+}
+
+
+struct Dimension4 {
+    let active: Set<Coordinate4>
+    
+    init( active: [Coordinate4] ) {
+        self.active = Set( active )
+    }
+
+    init( active: Dimension3 ) {
+        self.active = Set( active.active.map { Coordinate4( x: $0.x, y: $0.y, z: $0.z, w: 0 ) } )
+    }
+    
+    var cycle: Dimension4 {
+        let xmin = active.min( by: { $0.x < $1.x } )!.x - 1
+        let xmax = active.max( by: { $0.x < $1.x } )!.x + 1
+        let ymin = active.min( by: { $0.y < $1.y } )!.y - 1
+        let ymax = active.max( by: { $0.y < $1.y } )!.y + 1
+        let zmin = active.min( by: { $0.z < $1.z } )!.z - 1
+        let zmax = active.max( by: { $0.z < $1.z } )!.z + 1
+        let wmin = active.min( by: { $0.w < $1.w } )!.w - 1
+        let wmax = active.max( by: { $0.w < $1.w } )!.w + 1
+        var next: [Coordinate4] = []
+        
+        for x in xmin ... xmax {
+            for y in ymin ... ymax {
+                for z in zmin ... zmax {
+                    for w in wmin ... wmax {
+                        let current = Coordinate4( x: x, y: y, z: z, w: w )
+                        let neighbors = countNeighbors( coordinate: current )
+                        
+                        if active.contains( current ) {
+                            if neighbors == 3 || neighbors == 4 {
+                                next.append( current )
+                            }
+                        } else {
+                            if neighbors == 3 {
+                                next.append( current )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return Dimension4( active: next )
+    }
+    
+    func countNeighbors( coordinate: Coordinate4 ) -> Int {
+        var sum = 0
+        for x in coordinate.x - 1 ... coordinate.x + 1 {
+            for y in coordinate.y - 1 ... coordinate.y + 1 {
+                for z in coordinate.z - 1 ... coordinate.z + 1 {
+                    for w in coordinate.w - 1 ... coordinate.w + 1 {
+                        sum += active.contains( Coordinate4( x: x, y: y, z: z, w: w ) ) ? 1 : 0
+                    }
                 }
             }
         }
@@ -79,9 +145,10 @@ struct Dimension {
 
 
 let inputFile = "/Users/markj/Development/adventofcode/2020/input/day17.txt"
-var dimension = try Dimension( input: String( contentsOfFile: inputFile ) )
+var dimension3 = try Dimension3( input: String( contentsOfFile: inputFile ) )
+var dimension4 = Dimension4( active: dimension3 )
 
-for _ in 1 ... 6 { dimension = dimension.cycle }
+for _ in 1 ... 6 { dimension3 = dimension3.cycle; dimension4 = dimension4.cycle }
 
-print( "Part 1: \(dimension.active.count)" )
-//print( "Part 2: \(spoken[part2Limit-1])" )
+print( "Part 1: \(dimension3.active.count)" )
+print( "Part 2: \(dimension4.active.count)" )
