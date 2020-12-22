7cb9cf03312dd13cf2cda6bff4686495faa0949f
Mark Johnson <markj@matzsoft.com>
2020-12-21 17:54:08 -0800

    Vast simplification to prepare for part2.

--- 2020/day20.swift
+++ 2020/day20.swift
@@ -8,121 +8,80 @@
 
 import Foundation
 
-struct Configuration {
-    let top: String
-    let right: String
-    let bottom: String
-    let left: String
-    
-    init( top: String, right: String, bottom: String, left: String ) {
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.left = left
-    }
-    
-    init( image: [String] ) {
-        top = image.first!
-        bottom = image.last!
-        right = image.map { String( $0.last! ) }.joined()
-        left = image.map { String( $0.first! ) }.joined()
-    }
-    
-    var rotated: Configuration {
-        return Configuration(
-            top: String( left.reversed() ),
-            right: top,
-            bottom: String( right.reversed() ),
-            left: bottom
-        )
-    }
-    
-    var flipped: Configuration {
-        return Configuration(
-            top: String( top.reversed() ),
-            right: left,
-            bottom: String( bottom.reversed() ),
-            left: right
-        )
-    }
-}
-
-
-struct Tile: Equatable {
+struct Tile {
     let id: Int
     let image: [String]
-    let configurations: [Configuration]
-    let top: Set<String>
-    let right: Set<String>
-    let bottom: Set<String>
-    let left: Set<String>
+    let borders: Set<String>
 
+    static func rotated( by: Int, image: [String ] ) -> String {
+        let by = by % 4
+        
+        switch by {
+        case 0:
+            return image.first!
+        case 1:
+            return String( image.map { String( $0.first! ) }.joined().reversed() )
+        case 2:
+            return String( image.last!.reversed() )
+        case 3:
+            return image.map { String( $0.last! ) }.joined()
+        default:
+            return ""
+        }
+    }
+    
     init( input: String ) {
         let lines = input.split( separator: "\n" )
-        var configurations: [Configuration] = []
+        var tops: [String] = []
         
         id = Int( lines[0].dropFirst( 5 ).dropLast() )!
         image = lines.dropFirst().map { String( $0 ) }
         
-        configurations.append( Configuration( image: image ) )      // As input, no flip, no rotation
-        configurations.append( configurations.first!.rotated )      // No flip, rotatated right once
-        configurations.append( configurations.last!.rotated )       // No flip, rotatated right twice
-        configurations.append( configurations.last!.rotated )       // No flip, rotatated right three times
-        configurations.append( configurations.first!.flipped )      // Flipped, no rotation
-        configurations.append( configurations.last!.rotated )       // Flipped, rotatated right once
-        configurations.append( configurations.last!.rotated )       // Flipped, rotatated right twice
-        configurations.append( configurations.last!.rotated )       // Flipped, rotatated right three times
-        self.configurations = configurations
+        tops.append( Tile.rotated( by: 0, image: image ) )
+        tops.append( Tile.rotated( by: 1, image: image ) )
+        tops.append( Tile.rotated( by: 2, image: image ) )
+        tops.append( Tile.rotated( by: 3, image: image ) )
         
-        top = configurations.reduce( into: Set<String>(), { $0.insert( $1.top ) } )
-        right = configurations.reduce( into: Set<String>(), { $0.insert( $1.right ) } )
-        bottom = configurations.reduce( into: Set<String>(), { $0.insert( $1.bottom ) } )
-        left = configurations.reduce( into: Set<String>(), { $0.insert( $1.left ) } )
-    }
-    
-    static func == ( lhs: Tile, rhs: Tile ) -> Bool {
-        return lhs.id == rhs.id
+        let flipped = image.map { String( $0.reversed() ) }
+        
+        tops.append( Tile.rotated( by: 0, image: flipped ) )
+        tops.append( Tile.rotated( by: 1, image: flipped ) )
+        tops.append( Tile.rotated( by: 2, image: flipped ) )
+        tops.append( Tile.rotated( by: 3, image: flipped ) )
+        
+        borders = Set( tops )
     }
 }
 
 struct Node {
     let tile: Tile
-    let top: Set<Int>
-    let right: Set<Int>
-    let bottom: Set<Int>
-    let left: Set<Int>
+    let neighbors: Set<Int>
     
     init( tile: Tile, tiles: [Tile] ) {
         let others = tiles.filter { $0.id != tile.id }
         
         self.tile = tile
-        top = others.reduce( into: Set<Int>(), { set, other in
-            if !tile.top.intersection( other.bottom ).isEmpty { set.insert( other.id ) }
-        } )
-        right = others.reduce( into: Set<Int>(), { set, other in
-            if !tile.right.intersection( other.left ).isEmpty { set.insert( other.id ) }
-        } )
-        bottom = others.reduce( into: Set<Int>(), { set, other in
-            if !tile.bottom.intersection( other.top ).isEmpty { set.insert( other.id ) }
-        } )
-        left = others.reduce( into: Set<Int>(), { set, other in
-            if !tile.left.intersection( other.right ).isEmpty { set.insert( other.id ) }
+        neighbors = others.reduce( into: Set<Int>(), { set, other in
+            if !tile.borders.intersection( other.borders ).isEmpty { set.insert( other.id ) }
         } )
     }
+    
+    var isCorner: Bool { neighbors.count == 2 }
+    var isEdge: Bool { neighbors.count == 3 }
+    var isMiddle: Bool { neighbors.count == 4 }
 }
 
 
 func part1( nodes: [ Int: Node ] ) -> Int {
-    let corners = nodes.values.filter { $0.top.count == 2 }
+    let corners = nodes.values.filter { $0.isCorner }
 
     return corners.reduce( 1 ) { $0 * $1.tile.id }
 }
 
 
 let inputFile = "/Users/markj/Development/adventofcode/2020/input/day20.txt"
-let tiles =  try String( contentsOfFile: inputFile ).components( separatedBy: "\n\n" ).map {
-    Tile( input: $0 )
-}
+let groups =  try String( contentsOfFile: inputFile ).components( separatedBy: "\n\n" )
+let tiles =  groups.map { Tile( input: $0 ) }
 var nodes = tiles.reduce(into: [ Int : Node ](), { $0[$1.id] = Node( tile: $1, tiles: tiles ) } )
 
 //for id in tiles.map( { $0.id } ).sorted() {
