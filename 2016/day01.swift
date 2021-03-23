47138c047296a05c9671b0c07177b3fc9e736fa3
Mark Johnson <markj@matzsoft.com>
2021-03-23 15:02:35 -0700

    Updated to the adventOfCode tool.

--- 2016/day01.swift
+++ 2016/day01.swift
@@ -1,143 +1,93 @@
 //
-//  main.swift
-//  day01
-//
-//  Created by Mark Johnson on 11/25/18.
-//  Copyright © 2018 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day01 -
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 03/23/21 12:54:23
 //
 
 import Foundation
 
-enum Direction {
-    case North
-    case South
-    case East
-    case West
-}
 
-struct Point {
-    var x: Int
-    var y: Int
-}
-
-struct Vector {
-    let start: Point
-    let end: Point
-    let isNorthSouth: Bool
+enum Turn: Int { case right = 1, left = -1 }
+enum Direction: Int, CaseIterable {
+    case north, east, south, west
     
-    init(start: Point, end: Point) {
-        self.start = start
-        self.end = end
-        isNorthSouth = start.x == end.x
-    }
-    
-    func contains(x: Int) -> Bool {
-        return min( start.x, end.x ) <= x && x <= max( start.x, end.x )
-    }
-    
-    func contains(y: Int) -> Bool {
-        return min( start.y, end.y ) <= y && y <= max( start.y, end.y )
-    }
-    
-    // This function assumes all vectors are orthoganl, i.e. either North-South or East-West
-    func intersection(other: Vector) -> Point? {
-        if self.isNorthSouth && !other.isNorthSouth {
-            if other.contains(x: start.x) && contains(y: other.start.y) {
-                return Point( x: start.x, y: other.start.y )
-            }
-        } else if !self.isNorthSouth && other.isNorthSouth {
-            if other.contains(y: start.y) && contains(x: other.start.x) {
-                return Point( x: other.start.x, y: start.y )
-            }
-        } else if self.isNorthSouth && other.isNorthSouth {
-            if start.x == other.start.x {
-                if contains(y: other.start.y) {
-                    return other.start
-                } else if other.start.y <= min(start.y, end.y) {
-                    if other.end.y >= min(start.y, end.y) {
-                        return Point(x: start.x, y: min(start.y, end.y))
-                    }
-                } else {
-                    if other.end.y <= max(start.y, end.y) {
-                        return Point(x: start.x, y: max(start.y, end.y))
-                    }
-                }
-            }
-        } else {
-            // Both are East-West
-            if start.y == other.start.y {
-                if contains(x: other.start.x) {
-                    return other.start
-                } else if other.start.x <= min(start.x, end.x) {
-                    if other.end.x >= min(start.x, end.x) {
-                        return Point(x: min(start.x, end.x), y: start.y)
-                    }
-                } else {
-                    if other.end.x <= max(start.x, end.x) {
-                        return Point(x: max(start.x, end.x), y: start.y)
-                    }
-                }
-            }
-        }
-        
-        return nil
-    }
-}
-
-var direction = Direction.North
-var position = Point( x: 0, y: 0 )
-var path: [Vector] = []
-
-
-let leftChange = [
-    Direction.North : Direction.West,
-    Direction.South : Direction.East,
-    Direction.East : Direction.North,
-    Direction.West : Direction.South
-]
-let rightChange = [
-    Direction.North : Direction.East,
-    Direction.South : Direction.West,
-    Direction.East : Direction.South,
-    Direction.West : Direction.North
-]
-let modify = [
-    Direction.North : { ( blocks: Int ) -> Void in position.y += blocks },
-    Direction.South : { ( blocks: Int ) -> Void in position.y -= blocks },
-    Direction.East : { ( blocks: Int ) -> Void in position.x += blocks },
-    Direction.West : { ( blocks: Int ) -> Void in position.x -= blocks }
-]
-
-let seperator = ", "
-let input = """
-L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2
-"""
-let steps = input.split() { seperator.contains($0) }
-
-for step in steps {
-    if step.first == "L" {
-        direction = leftChange[direction]!
-    } else if step.first == "R" {
-        direction = rightChange[direction]!
-    }
-    
-    let index = step.index(step.startIndex, offsetBy: 1)
-    let string = step.suffix(from: index)
-    let blocks = Int( string )
-    let start = position
-    
-    modify[direction]!( blocks! )
-    path.append( Vector( start: start, end: position ) )
-}
-
-print( "Part1:", abs(position.x) + abs(position.y) )
-
-OUTER:
-for j in 1 ..< path.count {
-    for i in 0 ..< j - 1 {
-        if let hq = path[i].intersection(other: path[j]) {
-            print( "Part2:", abs(hq.x) + abs(hq.y) )
-            break OUTER
+    var vector: Point {
+        switch self {
+        case .north:
+            return Point( x: 0, y: 1 )
+        case .east:
+            return Point( x: 1, y: 0 )
+        case .south:
+            return Point( x: 0, y: -1 )
+        case .west:
+            return Point( x: -1, y: 0 )
         }
     }
+    
+    func turn( direction: Turn ) -> Direction {
+        let newValue = self.rawValue + direction.rawValue + Direction.allCases.count
+        return Direction( rawValue: newValue % Direction.allCases.count )!
+    }
 }
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+func +(lhs: Point, rhs: Point) -> Point {
+    return Point( x: lhs.x + rhs.x, y: lhs.y + rhs.y )
+}
+
+struct Step {
+    let turn: Turn
+    let distance: Int
+    
+    init( value: Substring ) {
+        turn = value.first! == "R" ? Turn.right : Turn.left
+        distance = Int( value.dropFirst() )!
+    }
+}
+
+
+func parse( input: AOCinput ) -> [Point] {
+    let steps = input.line.split() { ", ".contains( $0 ) }.map { Step( value: $0 ) }
+    var path = [ Point( x: 0, y: 0 ) ]
+    var direction = Direction.north
+    
+    for step in steps {
+        direction = direction.turn( direction: step.turn )
+        for _ in ( 0 ..< step.distance ) {
+            path.append( path.last! + direction.vector )
+        }
+    }
+    return path
+}
+
+
+func part1( input: AOCinput ) -> String {
+    let path = parse( input: input )
+    return "\(abs(path.last!.x) + abs(path.last!.y))"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let path = parse( input: input )
+    var seen = Set<Point>()
+    
+    for point in path {
+        if seen.contains( point ) {
+            return "\(abs(point.x) + abs(point.y))"
+        }
+        seen.insert( point )
+    }
+    return ""
+}
+
+
+try runTests( part1: part1, part2: part2 )
+try runSolutions( part1: part1, part2: part2 )
