48fc161d3189c27ff24a55c819aba38e3d797115
Mark Johnson <markj@matzsoft.com>
2021-03-25 18:44:18 -0700

    Added Crypto.swift to the library.
    Initially contains only the md5Hash function.

---
+++ tools/adventOfCode/Sources/adventOfCode/Library/Crypto.swift
@@ -0,0 +1,20 @@
+//
+//  Crypto.swift
+//  day05
+//
+//  Created by Mark Johnson on 3/25/21.
+//
+
+import Foundation
+import CommonCrypto
+
+
+func md5Hash( str: String ) -> String {
+    guard let string = str.data( using: .utf8 ) else { return "" }
+    
+    var digest = [UInt8]( repeating: 0, count: Int( CC_MD5_DIGEST_LENGTH ) )
+    
+    _ = string.withUnsafeBytes { CC_MD5( $0.baseAddress, UInt32( string.count ), &digest ) }
+
+    return digest.map { String( format: "%02x", UInt8( $0 ) ) }.joined()
+}
