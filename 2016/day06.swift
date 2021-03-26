aeffe42c0c1514f196e6e356516668cf6674be19
Mark Johnson <markj@matzsoft.com>
2021-03-25 20:04:09 -0700

    Last commit was premature.
    Forgot to "adventOfCode close day06" before committing.

--- 2016/day06.swift
+++ 2016/day06.swift
@@ -1,6 +1,6 @@
 //
 //         FILE: main.swift
-//  DESCRIPTION: day06 -
+//  DESCRIPTION: day06 - Santa sending a message via repetition code
 //        NOTES: ---
 //       AUTHOR: Mark T. Johnson, markj@matzsoft.com
 //    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
@@ -11,20 +11,30 @@
 import Foundation
 
 
-func parse( input: AOCinput ) -> Any? {
-    return nil
+func parse( input: AOCinput ) -> [[Character:Int]] {
+    let lines = input.lines
+    let columns: [[Character:Int]] = Array(repeating: [:], count: lines[0].count )
+
+    return input.lines.reduce( into: columns ) { columns, line in
+        line.enumerated().forEach {  ( index, letter ) in
+            columns[index][letter] = ( columns[index][letter] ?? 0 ) + 1
+            columns[index][letter] = columns[index][letter, default: 0] + 1
+        }
+    }
 }
 
 
 func part1( input: AOCinput ) -> String {
-    let something = parse( input: input )
-    return ""
+    let columns = parse( input: input )
+
+    return columns.map { $0.max( by: { $0.1 < $1.1 } )!.key }.map { String( $0 ) }.joined()
 }
 
 
 func part2( input: AOCinput ) -> String {
-    let something = parse( input: input )
-    return ""
+    let columns = parse( input: input )
+
+    return columns.map { $0.min( by: { $0.1 < $1.1 } )!.key }.map { String( $0 ) }.joined()
 }
 
 
