3208f5f98e8ae6698ab681b606827f274a4f9e9f
Mark Johnson <markj@matzsoft.com>
2021-03-26 23:56:47 -0700

    Added Extensions to the library.
    This was to add the tokenize extension to String.  The tokenize method
    is similar to split but keeps the delimiters in the result array.

---
+++ tools/adventOfCode/Sources/adventOfCode/Library/Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  Extensions.swift
+//  day07
+//
+//  Created by Mark Johnson on 3/26/21.
+//  Contains useful extensions to standard types.
+
+import Foundation
+
+extension Collection {
+    func splitAt( isSplit: ( Iterator.Element ) throws -> Bool ) rethrows -> [SubSequence] {
+        let indexes = try indices.filter( { try isSplit( self[$0] ) } )
+        var p = self.startIndex
+        var result = [SubSequence]()
+        
+        for index in indexes {
+            if index > p {
+                result.append( self[p..<index] )
+            }
+            result.append( self[index...index] )
+            p = self.index( after: index )
+        }
+        if p != self.endIndex {
+            result.append( suffix( from: p ) )
+        }
+        return result
+    }
+}
+
+extension String {
+    /**
+     Splits a string with multiple delimiters, but keeps the delimiters in the result.
+     
+     For example these 2 lines produce similar results.
+     
+     ~~~
+    let withoutDelimiters = string.split( whereSeparator: { delimiters.contains( $0 ) } )
+    let withDelimiters = string.tokenize( delimiters: delimiters )
+     ~~~
+
+     - Parameter delimiters: A string of characters, any one of which splits the input string.
+
+     - Returns: An array of Substring that keeps the delimiters interspersed with the other parts.
+    **/
+    func tokenize( delimiters: String ) -> [Substring] {
+        return self.splitAt( isSplit: { delimiters.contains( $0 ) } )
+    }
+}
