8205aadcb713d7576e25933111444fea9c35fad1
Mark Johnson <markj@matzsoft.com>
2021-04-20 17:17:44 -0700

    Converted 2017 day14 to use the library.
    Also had to:
    1. Add KnotHash.swift to the library.
    2. Update day10 to accomodate that change.

--- 2017/day10.swift
+++ 2017/day10.swift
@@ -10,56 +10,9 @@
 
 import Foundation
 
-class KnotHash {
-    var list: [Int]
-    let lengths: [Int]
-    var current: Int
-    var skip: Int
-    
-    init( list: [Int], lengths: [Int] ) {
-        self.list = list
-        self.lengths = lengths
-        current = 0
-        skip = 0
-    }
-    
-    func oneRound() -> Void {
-        for length in lengths {
-            if current + length  < list.count {
-                let reversed = Array( list[ current ..< ( current + length) ].reversed() )
-                
-                list.replaceSubrange( current ..< ( current + length), with: reversed )
-            } else {
-                let working = list + list
-                let reversed = Array( working[ current ..< ( current + length) ].reversed() )
-                let part1 = Array( reversed[ ( list.count - current )...] )
-                let part2 = Array( list[ ( ( current + length ) % list.count ) ..< current ] )
-                let part3 = Array( reversed[ 0 ..< ( list.count - current ) ] )
-                
-                list = part1 + part2 + part3
-            }
-            
-            current = ( current + length + skip ) % list.count
-            skip += 1
-        }
-    }
-    
-    func generate() -> String {
-        guard list.count == 256 else { return "Wrong sized list" }
-        
-        for _ in 0 ..< 64 { oneRound() }
-        
-        return stride( from: 0, to: list.count, by: 16 ).map {
-            return String( format: "%02x", list[ $0 ..< ( $0 + 16 ) ].reduce( 0, { $0 ^ $1 } ) )
-        }.joined()
-    }
-}
-
 
 func part1( input: AOCinput ) -> String {
-    let list = ( 0 ... 255 ).map { $0 }
-    let lengths = input.line.split( separator: "," ).map { Int( $0 )! }
-    let knot = KnotHash( list: list, lengths: lengths )
+    let knot = KnotHash( lengths: input.line.split( separator: "," ).map { Int( $0 )! } )
 
     knot.oneRound()
     return "\(knot.list[0] * knot.list[1])"
@@ -67,9 +20,7 @@ func part1( input: AOCinput ) -> String {
 
 
 func part2( input: AOCinput ) -> String {
-    let list = ( 0 ... 255 ).map { $0 }
-    let lengths = input.line.map { Int( $0.unicodeScalars.first!.value ) } + [ 17, 31, 73, 47, 23 ]
-    let knothash = KnotHash( list: list, lengths: lengths )
+    let knothash = KnotHash( input: input.line )
     
     return "\(knothash.generate())"
 }
---
+++ tools/adventOfCode/Sources/adventOfCode/Library/KnotHash.swift
@@ -0,0 +1,63 @@
+//
+//  KnotHash.swift
+//  day10
+//
+//  Created by Mark Johnson on 4/19/21.
+//
+
+import Foundation
+
+class KnotHash {
+    static let extraLengths = [ 17, 31, 73, 47, 23 ]
+    
+    var list: [Int]
+    let lengths: [Int]
+    var current: Int
+    var skip: Int
+    
+    init( lengths: [Int] ) {
+        list = ( 0 ... 255 ).map { $0 }
+        self.lengths = lengths
+        current = 0
+        skip = 0
+    }
+    
+    init( input: String ) {
+        list = ( 0 ... 255 ).map { $0 }
+        lengths = input.map { Int( $0.unicodeScalars.first!.value ) } + KnotHash.extraLengths
+        current = 0
+        skip = 0
+    }
+
+    
+    func oneRound() -> Void {
+        for length in lengths {
+            if current + length  < list.count {
+                let reversed = Array( list[ current ..< ( current + length) ].reversed() )
+                
+                list.replaceSubrange( current ..< ( current + length), with: reversed )
+            } else {
+                let working = list + list
+                let reversed = Array( working[ current ..< ( current + length) ].reversed() )
+                let part1 = Array( reversed[ ( list.count - current )...] )
+                let part2 = Array( list[ ( ( current + length ) % list.count ) ..< current ] )
+                let part3 = Array( reversed[ 0 ..< ( list.count - current ) ] )
+                
+                list = part1 + part2 + part3
+            }
+            
+            current = ( current + length + skip ) % list.count
+            skip += 1
+        }
+    }
+    
+    func generate() -> String {
+        guard list.count == 256 else { return "Wrong sized list" }
+        
+        for _ in 0 ..< 64 { oneRound() }
+        
+        return stride( from: 0, to: list.count, by: 16 ).map {
+            return String( format: "%02x", list[ $0 ..< ( $0 + 16 ) ].reduce( 0, { $0 ^ $1 } ) )
+        }.joined()
+    }
+}
