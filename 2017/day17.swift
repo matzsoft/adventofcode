608de6a74eed41b44102a4998209f1f9bcb794c6
Mark Johnson <markj@matzsoft.com>
2021-04-22 19:40:13 -0700

    Converted 2017 day17 to use the library.

--- 2017/day17.swift
+++ 2017/day17.swift
@@ -1,39 +1,50 @@
 //
-//  main.swift
-//  day17
-//
-//  Created by Mark Johnson on 1/15/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day17 - Spinlock
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 04/22/21 19:24:01
 //
 
 import Foundation
 
-let input = 301
 
-let step = input
-var buffer = [ 0 ]
-var current = 0
+func part1( input: AOCinput ) -> String {
+    let step = Int( input.line )!
+    var buffer = [ 0 ]
+    var current = 0
 
-for value in 1 ... 2017 {
-    current = ( current + step ) % buffer.count + 1
-    buffer.insert( value, at: current )
-}
-
-let desired = ( current + 1 ) % buffer.count
-
-print( "Part1:", buffer[desired] )
-
-
-var bufferLen = buffer.count
-var last = buffer[1]
-
-for value in 2018 ... 50000000 {
-    current = ( current + step ) % bufferLen + 1
-    bufferLen += 1
-    
-    if current == 1 {
-        last = value
+    for value in 1 ... 2017 {
+        current = ( current + step ) % buffer.count + 1
+        buffer.insert( value, at: current )
     }
+
+    let desired = ( current + 1 ) % buffer.count
+
+    return "\(buffer[desired])"
 }
 
-print( "Part2:", last )
+
+func part2( input: AOCinput ) -> String {
+    let step = Int( input.line )!
+    var current = 0
+    var value = 1
+    var last = 0
+    
+    while value <= 50000000 {
+        current = ( current + step ) % value + 1
+        
+        if current == 1 {
+            last = value
+        }
+        value += 1
+    }
+
+    return "\(last)"
+}
+
+
+try runTests( part1: part1, part2: part2 )
+try runSolutions( part1: part1, part2: part2 )
