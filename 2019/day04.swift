abf5353548d30986b847d3529e4c0e3f5df9f84c
Mark Johnson <markj@matzsoft.com>
2021-05-31 23:31:06 -0700

    Converted 2019 day04 to use the library.

--- 2019/day04.swift
+++ 2019/day04.swift
@@ -1,63 +1,76 @@
 //
-//  main.swift
-//  day04
-//
-//  Created by Mark Johnson on 12/3/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day04 - Secure Container
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 05/31/21 22:55:02
 //
 
 import Foundation
 
-let input = "193651-649729"
-let bounds = input.split( separator: "-" ).map { Int( $0 )! }
-var validCount1 = 0
-var validCount2 = 0
 
-func isValid1( number: Int ) -> Bool {
-    let digits = Array( String( number ) ).map { Int( String( $0 ) )! }
-    var duplicate = false
-    var last = 0
-    
-    for digit in digits {
-        if digit < last {
-            return false
-        }
-        if digit == last {
-            duplicate = true
-        }
-        last = digit
-    }
-    
-    return duplicate
+func parse( input: AOCinput ) -> [Int] {
+    return input.line.split( separator: "-" ).map { Int( $0 )! }
 }
 
-func isValid2( number: Int ) -> Bool {
-    let digits = Array( String( number ) ).map { Int( String( $0 ) )! }
-    var duplicateCount = 1
-    var last = 0
-    
-    for digit in digits {
-        if digit == last {
-            duplicateCount += 1
-        } else if duplicateCount == 2 {
-            return true
-        } else {
-            duplicateCount = 1
-        }
-        last = digit
-    }
-    
-    return duplicateCount == 2
-}
 
-for number in bounds[0] ... bounds[1] {
-    if isValid1( number: number ) {
-        validCount1 += 1
-        if isValid2( number: number ) {
-            validCount2 += 1
+func findWithDuplicate( bounds: [Int] ) -> [Int] {
+    return ( bounds[0] ... bounds[1] ).filter {
+        let digits = Array( String( $0 ) )
+        var duplicate = false
+        var last: Character = " "
+        
+        for digit in digits {
+            if digit < last {
+                return false
+            }
+            if digit == last {
+                duplicate = true
+            }
+            last = digit
         }
+        
+        return duplicate
     }
 }
 
-print( "Part 1: \(validCount1)" )
-print( "Part 2: \(validCount2)" )
+
+func part1( input: AOCinput ) -> String {
+    let bounds = parse( input: input )
+    let list = findWithDuplicate( bounds: bounds )
+    
+    return "\(list.count)"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let bounds = parse( input: input )
+    let list = findWithDuplicate( bounds: bounds ).filter {
+        let digits = Array( String( $0 ) )
+        var duplicateCount = 1
+        var last: Character = " "
+        
+        for digit in digits {
+            if digit == last {
+                duplicateCount += 1
+            } else if duplicateCount == 2 {
+                return true
+            } else {
+                duplicateCount = 1
+            }
+            last = digit
+        }
+        
+        return duplicateCount == 2
+    }
+    
+    return "\(list.count)"
+}
+
+
+try runTestsPart1( part1: part1 )
+try runTestsPart2( part2: part2 )
+try runPart1( part1: part1 )
+try runPart2( part2: part2 )
