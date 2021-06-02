cea694204e5db8f33d62f9f412549efdaa3555de
Mark Johnson <markj@matzsoft.com>
2021-06-01 17:25:29 -0700

    Converted 2019 day09 to use the library.
    Also had to expand Intcode.swift with new capabilities.
    1. relativeBase and supporting opcode and parameter mode.
    2. Expanding memory.

--- 2019/day09.swift
+++ 2019/day09.swift
@@ -1,200 +1,45 @@
 //
-//  main.swift
-//  day09
-//
-//  Created by Mark Johnson on 12/8/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day09 - Sensor Boost
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/01/21 15:56:15
 //
 
 import Foundation
 
-extension String: Error {}
 
-enum Opcode: Int {
-    case add                = 1
-    case multiply           = 2
-    case input              = 3
-    case output             = 4
-    case jumpIfTrue         = 5
-    case jumpIfFalse        = 6
-    case lessThan           = 7
-    case equals             = 8
-    case relativeBaseOffset = 9
-    case halt               = 99
+func parse( input: AOCinput ) -> Intcode {
+    let initialMemory = input.line.split( separator: "," ).map { Int( $0 )! }
+    return Intcode( name: "BOOST", memory: initialMemory )
 }
 
-enum ParameterMode: Int {
-    case position  = 0
-    case immediate = 1
-    case relative  = 2
-}
 
-struct Instruction {
-    let opcode: Opcode
-    let modes: [ParameterMode]
-
-    init( instruction: Int ) {
-        opcode = Opcode( rawValue: instruction % 100 )!
-        modes = [
-            ParameterMode( rawValue: instruction /   100 % 10 )!,
-            ParameterMode( rawValue: instruction /  1000 % 10 )!,
-            ParameterMode( rawValue: instruction / 10000 % 10 )!,
-        ]
-    }
+func part1( input: AOCinput ) -> String {
+    let computer = parse( input: input )
     
-    func mode( operand: Int ) -> ParameterMode {
-        return modes[ operand - 1 ]
+    computer.inputs = [ 1 ]
+    while let output = try! computer.execute() {
+        return "\(output)"
     }
+    return "Failure"
 }
 
-class IntcodeComputer {
-    let name: String
-    var pc: Int
-    var relativeBase: Int
-    var memory: [Int]
-    var inputs: [Int]
-    var debug = false
+
+func part2( input: AOCinput ) -> String {
+    let computer = parse( input: input )
     
-    init( name: String, memory: [Int], inputs: [Int] ) {
-        self.name = name
-        pc = 0
-        relativeBase = 0
-        self.memory = memory
-        self.inputs = inputs
+    computer.inputs = [ 2 ]
+    while let output = try! computer.execute() {
+        return "\(output)"
     }
-
-    func fetch( _ instruction: Instruction, operand: Int ) throws -> Int {
-        var location = memory[ pc + operand ]
-        
-        switch instruction.mode( operand: operand ) {
-        case .position:
-            break
-        case .immediate:
-            return location
-        case .relative:
-            location += relativeBase
-        }
-
-        if location < 0 {
-            throw "Negative memory fetch (\(location)) at address \(pc)"
-        } else if location >= memory.count {
-            return 0
-        }
-        return memory[location]
-    }
-    
-    func store( _ instruction: Instruction, operand: Int, value: Int ) throws -> Void {
-        var location = memory[ pc + operand ]
-        
-        switch instruction.mode( operand: operand ) {
-        case .position:
-            break
-        case .immediate:
-            throw "Immediate mode invalid for address \(pc)"
-        case .relative:
-            location += relativeBase
-        }
-
-        if location < 0 {
-            throw "Negative memory store (\(location)) at address \(pc)"
-        } else if location >= memory.count {
-            memory.append( contentsOf: Array( repeating: 0, count: location - memory.count + 1 ) )
-        }
-        memory[location] = value
-    }
-
-    func grind() -> Int? {
-        while true {
-            let instruction = Instruction( instruction: memory[pc] )
-            
-            switch instruction.opcode {
-            case .add:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-                
-                try! store( instruction, operand: 3, value: operand1 + operand2 )
-                pc += 4
-            case .multiply:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-
-                try! store( instruction, operand: 3, value: operand1 * operand2 )
-                pc += 4
-            case .input:
-                if debug { print( "\(name): inputs \(inputs.first!)" ) }
-                try! store( instruction, operand: 1, value: inputs.removeFirst() )
-                pc += 2
-            case .output:
-                let operand1 = try! fetch( instruction, operand: 1 )
-
-                pc += 2
-                if debug { print( "\(name): outputs \(operand1)" ) }
-                return operand1
-            case .jumpIfTrue:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-
-                if operand1 != 0 {
-                    pc = operand2
-                } else {
-                    pc += 3
-                }
-            case .jumpIfFalse:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-
-                if operand1 == 0 {
-                    pc = operand2
-                } else {
-                    pc += 3
-                }
-            case .lessThan:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-
-                if operand1 < operand2 {
-                    try! store( instruction, operand: 3, value: 1 )
-                } else {
-                    try! store( instruction, operand: 3, value: 0 )
-                }
-                pc += 4
-            case .equals:
-                let operand1 = try! fetch( instruction, operand: 1 )
-                let operand2 = try! fetch( instruction, operand: 2 )
-
-                if operand1 == operand2 {
-                    try! store( instruction, operand: 3, value: 1 )
-                } else {
-                    try! store( instruction, operand: 3, value: 0 )
-                }
-                pc += 4
-            case .relativeBaseOffset:
-                let operand1 = try! fetch( instruction, operand: 1 )
-
-                relativeBase += operand1
-                pc += 2
-            case .halt:
-                if debug { print( "\(name): halts" ) }
-                return nil
-            }
-        }
-    }
+    return "Failure"
 }
 
 
-
-guard CommandLine.arguments.count > 1 else {
-    print( "No input file specified" )
-    exit( 1 )
-}
-
-let input = try String( contentsOfFile: CommandLine.arguments[1] ).dropLast( 1 )
-let initialMemory = input.split( separator: "," ).map { Int($0)! }
-
-for part in 1 ... 2 {
-    let computer = IntcodeComputer( name: "HAL", memory: initialMemory, inputs: [ part ] )
-
-    while let output = computer.grind() {
-        print( "Part \(part): \(output)" )
-    }
-}
+try runTestsPart1( part1: part1 )
+try runTestsPart2( part2: part2 )
+try runPart1( part1: part1 )
+try runPart2( part2: part2 )
--- tools/adventOfCode/Sources/adventOfCode/Library/Intcode.swift
+++ tools/adventOfCode/Sources/adventOfCode/Library/Intcode.swift
@@ -9,101 +9,139 @@ import Foundation
 
 class Intcode {
     struct Instruction {
-        enum Mode: Int { case  position = 0, immediate = 1 }
+        enum Mode: Int { case  position = 0, immediate = 1, relative = 2 }
         enum Opcode: Int {
             case add = 1, multiply = 2, input = 3, output = 4
-            case jumptrue = 5, jumpfalse = 6, less = 7, equals = 8, halt = 99
+            case jumpIfTrue = 5, jumpIfFalse = 6, lessThan = 7, equals = 8
+            case relativeBaseOffset = 9, halt = 99
         }
         
         let opcode: Opcode
-        let parameter1Mode: Mode
-        let parameter2Mode: Mode
-        let parameter3Mode: Mode
+        let modes: [Mode]
 
         init( instruction: Int ) {
             opcode = Opcode( rawValue: instruction % 100 )!
-            parameter1Mode = Mode( rawValue: instruction /   100 % 10 )!
-            parameter2Mode = Mode( rawValue: instruction /  1000 % 10 )!
-            parameter3Mode = Mode( rawValue: instruction / 10000 % 10 )!
+            modes = [
+                Mode( rawValue: instruction /   100 % 10 )!,
+                Mode( rawValue: instruction /  1000 % 10 )!,
+                Mode( rawValue: instruction / 10000 % 10 )!,
+            ]
+        }
+        
+        func mode( operand: Int ) -> Mode {
+            return modes[ operand - 1 ]
         }
     }
 
     let name: String
     var ip: Int
+    var relativeBase: Int
     var memory: [Int]
     var inputs: [Int]
-    var outputs: [Int]
     var debug = false
 
     init( name: String, memory: [Int] ) {
         self.name = name
         ip = 0
+        relativeBase = 0
         self.memory = memory
         self.inputs = []
-        outputs = []
     }
 
-    func fetchOperand( address: Int, mode: Instruction.Mode ) -> Int {
-        let value = memory[address]
+    func fetch( _ instruction: Instruction, operand: Int ) throws -> Int {
+        var location = memory[ ip + operand ]
         
-        switch mode {
+        switch instruction.mode( operand: operand ) {
         case .position:
-            return memory[value]
+            break
         case .immediate:
-            return value
+            return location
+        case .relative:
+            location += relativeBase
         }
+
+        if location < 0 {
+            throw RuntimeError( "Negative memory fetch (\(location)) at address \(ip)" )
+        } else if location >= memory.count {
+            return 0
+        }
+        return memory[location]
+    }
+    
+    func store( _ instruction: Instruction, operand: Int, value: Int ) throws -> Void {
+        var location = memory[ ip + operand ]
+        
+        switch instruction.mode( operand: operand ) {
+        case .position:
+            break
+        case .immediate:
+            throw RuntimeError( "Immediate mode invalid for address \(ip)" )
+        case .relative:
+            location += relativeBase
+        }
+
+        if location < 0 {
+            throw RuntimeError( "Negative memory store (\(location)) at address \(ip)" )
+        } else if location >= memory.count {
+            memory.append( contentsOf: Array( repeating: 0, count: location - memory.count + 1 ) )
+        }
+        memory[location] = value
     }
 
-    func execute() -> Int? {
+    func execute() throws -> Int? {
         while true {
             let instruction = Instruction( instruction: memory[ip] )
             
             switch instruction.opcode {
             case .add:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
                 
-                memory[memory[ip+3]] = operand1 + operand2
+                try store( instruction, operand: 3, value: operand1 + operand2 )
                 ip += 4
             case .multiply:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
-                
-                memory[memory[ip+3]] = operand1 * operand2
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
+
+                try store( instruction, operand: 3, value: operand1 * operand2 )
                 ip += 4
             case .input:
                 if debug { print( "\(name): inputs \(inputs.first!)" ) }
-                memory[memory[ip+1]] = inputs.removeFirst()
+                try store( instruction, operand: 1, value: inputs.removeFirst() )
                 ip += 2
             case .output:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
+                let operand1 = try fetch( instruction, operand: 1 )
                 
-                outputs.append( operand1 )
                 ip += 2
                 if debug { print( "\(name): outputs \(operand1)" ) }
                 return operand1
-            case .jumptrue:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
+            case .jumpIfTrue:
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
                 
                 ip = operand1 != 0 ? operand2 : ip + 3
-            case .jumpfalse:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
-                
+            case .jumpIfFalse:
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
+
                 ip = operand1 == 0 ? operand2 : ip + 3
-            case .less:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
-                
-                memory[memory[ip+3]] = operand1 < operand2 ? 1 : 0
+            case .lessThan:
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
+
+                try store( instruction, operand: 3, value: operand1 < operand2 ? 1 : 0 )
                 ip += 4
             case .equals:
-                let operand1 = fetchOperand( address: ip + 1, mode: instruction.parameter1Mode )
-                let operand2 = fetchOperand( address: ip + 2, mode: instruction.parameter2Mode )
-                
-                memory[memory[ip+3]] = operand1 == operand2 ? 1 : 0
+                let operand1 = try fetch( instruction, operand: 1 )
+                let operand2 = try fetch( instruction, operand: 2 )
+
+                try store( instruction, operand: 3, value: operand1 == operand2 ? 1 : 0 )
                 ip += 4
+            case .relativeBaseOffset:
+                let operand1 = try fetch( instruction, operand: 1 )
+                
+                relativeBase += operand1
+                ip += 2
             case .halt:
                 if debug { print( "\(name): halts" ) }
                 return nil
