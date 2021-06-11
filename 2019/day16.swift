6de30f3de934043f17b26d2f83b0d3b6b2a99a74
Mark Johnson <markj@matzsoft.com>
2021-06-11 12:11:42 -0700

    Converted 2019 day16 to use the library.

--- 2019/day16.swift
+++ 2019/day16.swift
@@ -1,74 +1,61 @@
 //
-//  main.swift
-//  day16
-//
-//  Created by Mark Johnson on 12/16/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day16 - Flawed Frequency Transmission
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/05/21 12:57:29
 //
 
 import Foundation
 
 let basePattern = [ 0, 1, 0, -1 ]
 
-func repeatingPattern( signalIndex: Int ) -> [ Int ] {
-    return basePattern.flatMap { Array(repeating: $0, count: signalIndex + 1 ) }
-}
-
-func elementTerm( signal: [Int], signalIndex: Int, pattern: [Int] ) -> Int {
-    return signal[signalIndex] * pattern[ ( signalIndex + 1 ) % pattern.count ]
-}
-
-func newSignalElement( signal: [Int], signalIndex: Int ) -> Int {
-    let pattern = repeatingPattern( signalIndex: signalIndex )
-    let sum = signal.indices.reduce( 0, {
-        $0 + elementTerm( signal: signal, signalIndex: $1, pattern: pattern )
-    } )
-    
-    return abs( sum % 10 )
-}
-
-func phase( signal: [Int] ) -> [Int] {
-    return signal.indices.map { newSignalElement( signal: signal, signalIndex: $0 ) }
-}
-
 func signature( signal: [Int] ) -> String {
    return signal[ ..<8 ].map { String( $0 ) }.joined()
 }
 
-func phase2( signal: [Int] ) -> [Int] {
-    var sum = 0
-    
-    return signal.map { sum += $0; return sum % 10 }
+
+func parse( input: AOCinput ) -> [Int] {
+    return input.line.map { Int( String( $0 ) )! }
 }
 
-func signature2( signal: [Int] ) -> String {
+
+func part1( input: AOCinput ) -> String {
+    var signal = parse( input: input )
+    
+    for _ in 0 ..< 100 {
+        signal = signal.indices.map { index in
+            return abs( signal.enumerated().map { digit in
+                let pattern = basePattern[ ( ( digit.offset + 1 ) / ( index + 1 ) ) % basePattern.count ]
+                return digit.element * pattern
+            }.reduce( 0, + ) ) % 10
+        }
+    }
+    
+    return signature( signal: signal )
+}
+
+
+func part2( input: AOCinput ) -> String {
+    var signal = parse( input: input )
+    let msgOffset = signal[ ..<7 ].reduce( 0 ) { 10 * $0 + $1 }
+    let modOffset = msgOffset % signal.count
+    let fullLength = 10000 * signal.count
+    let repeats = ( fullLength - msgOffset ) / signal.count
+
+    signal = ( signal[modOffset...] + ( 0 ..< repeats ).flatMap { _ in signal } ).reversed()
+    for _ in 0 ..< 100 {
+        var sum = 0
+        signal = signal.map { sum += $0; return sum % 10 }
+    }
+    
     return signature( signal: signal.reversed() )
 }
 
 
-guard CommandLine.arguments.count > 1 else {
-    print( "No input file specified" )
-    exit( 1 )
-}
-
-//let input = "69317163492948606335995924319873"
-let input = try String( contentsOfFile: CommandLine.arguments[1] ).dropLast( 1 )
-let signal = input.map { Int( String( $0 ) )! }
-let msgOffset = signal[ ..<7 ].reduce( 0, { 10 * $0 + $1 } )
-var lastSignal = signal
-
-( 1 ... 100 ).forEach { _ in lastSignal = phase( signal: lastSignal ) }
-
-print( "Part 1: \( signature( signal: lastSignal ) )" )
-
-let part2Length = 10000 * signal.count
-var part2Signal = Array( signal[ ( msgOffset % signal.count )... ] )
-
-( 0 ..< ( ( part2Length - msgOffset ) / signal.count ) ).forEach { _ in
-    part2Signal.append( contentsOf: signal )
-}
-
-part2Signal.reverse()
-( 1 ... 100 ).forEach { _ in part2Signal = phase2( signal: part2Signal ) }
-
-print( "Part 2: \( signature2( signal: part2Signal ) )" )
+try runTestsPart1( part1: part1 )
+try runTestsPart2( part2: part2 )
+try runPart1( part1: part1 )
+try runPart2( part2: part2 )
