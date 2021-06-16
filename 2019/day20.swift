0b8b59a899a56d5fa1199cd2a55994b57624d14c
Mark Johnson <markj@matzsoft.com>
2021-06-15 18:44:49 -0700

    Converted 2019 day20 to use the library.

--- 2019/day20.swift
+++ 2019/day20.swift
@@ -1,129 +1,71 @@
 //
-//  main.swift
-//  day20
-//
-//  Created by Mark Johnson on 12/20/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day20 - Donut Maze
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/15/21 18:08:55
 //
 
 import Foundation
 
-struct Point: Hashable {
-    let x: Int
-    let y: Int
-    
-    static func +( lhs: Point, rhs: Point ) -> Point {
-        return Point( x: lhs.x + rhs.x, y: lhs.y + rhs.y )
-    }
-    
-    static func -( lhs: Point, rhs: Point ) -> Point {
-        return Point( x: lhs.x - rhs.x, y: lhs.y - rhs.y )
-    }
-    
-    func distance( other: Point ) -> Int {
-        return abs( x - other.x ) + abs( y - other.y )
-    }
-    
-    func hash( into hasher: inout Hasher ) {
-        hasher.combine( x )
-        hasher.combine( y )
-    }
-    
-    func min( other: Point ) -> Point {
-        return Point( x: Swift.min( x, other.x ), y: Swift.min( y, other.y ) )
-    }
-    
-    func max( other: Point ) -> Point {
-        return Point( x: Swift.max( x, other.x ), y: Swift.max( y, other.y ) )
-    }
-}
-
-enum Direction: Int, CaseIterable {
-    case north = 1, south = 2, west = 3, east = 4
-    
-    var vector: Point {
-        switch self {
-        case .north:
-            return Point( x: 0, y: -1 )
-        case .south:
-            return Point( x: 0, y: 1 )
-        case .west:
-            return Point( x: -1, y: 0 )
-        case .east:
-            return Point( x: 1, y: 0 )
-        }
-    }
-    
-    var reverse: Direction {
-        switch self {
-        case .north:
-            return .south
-        case .south:
-            return .north
-        case .west:
-            return .east
-        case .east:
-            return .west
-        }
-    }
-}
-
-enum PortalType {
-    case enter, leave, outside, inside
-    
-    var reverse: PortalType {
-        switch self {
-        case .enter:
-            return .leave
-        case .leave:
-            return .enter
-        case .outside:
-            return .inside
-        case .inside:
-            return .outside
-        }
-    }
-}
-
-struct Portal {
-    let label: String
-    let destination: Point
-    let type: PortalType
-}
-
 struct Donut {
-    let map: Set<Point>
-    let portals: [ Point : Portal ]
-    let enter: Point
-    let leave: Point
-    
-    init( input: String ) {
-        let grid = input.split( separator: "\n" ).map { Array( $0 ) }
-        var map = Set<Point>()
-        var portals: [ Point : Portal ] = [:]
-        var labels: [ String : Point ] = [:]
-        var enter: Point?
-        var leave: Point?
+    enum PortalType {
+        case enter, leave, outside, inside
         
-        func getLabel( grid: [[Substring.Element]], x: Int, y: Int ) -> ( String, Point, PortalType )? {
+        var reverse: PortalType {
+            switch self {
+            case .enter:
+                return .leave
+            case .leave:
+                return .enter
+            case .outside:
+                return .inside
+            case .inside:
+                return .outside
+            }
+        }
+    }
+
+    struct Portal {
+        let label: String
+        let destination: Point2D
+        let type: PortalType
+    }
+
+    let map: Set<Point2D>
+    let portals: [ Point2D : Portal ]
+    let enter: Point2D
+    let leave: Point2D
+    
+    init( lines: [String] ) {
+        let grid = lines.map { Array( $0 ) }
+        var map = Set<Point2D>()
+        var portals: [ Point2D : Portal ] = [:]
+        var labels: [ String : Point2D ] = [:]
+        var enter: Point2D?
+        var leave: Point2D?
+        
+        func getLabel( grid: [[Substring.Element]], x: Int, y: Int ) -> ( String, Point2D, PortalType )? {
             var label = ""
-            var point: Point?
+            var point: Point2D?
             
             if y < grid.count - 1 && grid[y+1][x].isUppercase {
                 label = String( grid[y][x] ) + String( grid[y+1][x] )
                 
                 if y > 0 && grid[y-1][x] == "." {
-                    point = Point( x: x, y: y-1 )
+                    point = Point2D( x: x, y: y-1 )
                 } else if y < grid.count - 2 && grid[y+2][x] == "." {
-                    point = Point( x: x, y: y+2 )
+                    point = Point2D( x: x, y: y+2 )
                 }
             } else if x < grid[y].count - 1 && grid[y][x+1].isUppercase {
                 label = String( grid[y][x] ) + String( grid[y][x+1] )
 
                 if x > 0 && grid[y][x-1] == "." {
-                    point = Point( x: x-1, y: y )
+                    point = Point2D( x: x-1, y: y )
                 } else if x < grid[y].count - 2 && grid[y][x+2] == "." {
-                    point = Point( x: x+2, y: y )
+                    point = Point2D( x: x+2, y: y )
                 }
             }
             
@@ -145,7 +87,7 @@ struct Donut {
                 case " ", "#":
                     break
                 case ".":
-                    map.insert( Point( x: x, y: y ) )
+                    map.insert( Point2D( x: x, y: y ) )
                 case let char where char.isUppercase:
                     if let ( label, point, type ) = getLabel( grid: grid, x: x, y: y ) {
                         switch label {
@@ -199,7 +141,7 @@ struct Donut {
                 }
             }
             
-            for move in Direction.allCases {
+            for move in Direction4.allCases {
                 let next = current + move.vector
                 
                 if map.contains( next ) && !seen.contains( next ) {
@@ -212,11 +154,11 @@ struct Donut {
     }
     
     struct Node {
-        var list: [ Point : Int ]
+        var list: [ Point2D : Int ]
     }
     
-    func getNetwork() -> [ Point : Node ] {
-        var network: [ Point : Node ] = [:]
+    func getNetwork() -> [ Point2D : Node ] {
+        var network: [ Point2D : Node ] = [:]
         
         for portal in portals {
             if portal.value.type != .leave {
@@ -226,7 +168,7 @@ struct Donut {
         return network
     }
     
-    func networkAsString( network: [ Point : Node ] ) -> String {
+    func networkAsString( network: [ Point2D : Node ] ) -> String {
         var result: [ String ] = []
         
         for key in network.keys.sorted( by: { portals[$0]!.label < portals[$1]!.label } ) {
@@ -239,13 +181,13 @@ struct Donut {
         return result.joined( separator: "\n" )
     }
     
-    func traverseSubnet( point: Point ) -> Node {
+    func traverseSubnet( point: Point2D ) -> Node {
         var subnet = Node( list: [:] )
-        var queue: [ ( Point, Int ) ] = []
+        var queue: [ ( Point2D, Int ) ] = []
         var seen = Set( [ point ] )
   
-        func moveOne( current: Point, distance: Int ) -> Void {
-            for move in Direction.allCases {
+        func moveOne( current: Point2D, distance: Int ) -> Void {
+            for move in Direction4.allCases {
                 let next = current + move.vector
                 
                 if map.contains( next ) && !seen.contains( next ) {
@@ -276,15 +218,15 @@ struct Donut {
     }
     
     struct Candidate {
-        let point: Point
+        let point: Point2D
         let distance: Int
         let level: Int
     }
     
     struct BeenThere {
-        var record: [ Point : Set<Int> ] = [:]
+        var record: [ Point2D : Set<Int> ] = [:]
 
-        mutating func check( point: Point, level: Int ) -> Bool {
+        mutating func check( point: Point2D, level: Int ) -> Bool {
             if let entry = record[point] {
                 if entry.contains( level ) {
                     return true
@@ -302,7 +244,7 @@ struct Donut {
         let network = getNetwork()
         var queue = [ Candidate( point: enter, distance: 0, level: 0 ) ]
         var seen = BeenThere()
-        var loopDetector: Set<[Point]> = []
+        var loopDetector: Set<[Point2D]> = []
         
         //print( networkAsString( network: network ) )
         while !queue.isEmpty {
@@ -359,13 +301,25 @@ struct Donut {
 }
 
 
-guard CommandLine.arguments.count > 1 else {
-    print( "No input file specified" )
-    exit( 1 )
+
+func parse( input: AOCinput ) -> Donut {
+    return Donut( lines: input.lines )
 }
 
-let input = try String( contentsOfFile: CommandLine.arguments[1] )
-let donut = Donut( input: input )
 
-print( "Part 1: \( donut.traverse() )" )
-print( "Part 2: \( donut.traverseRecurse() )" )
+func part1( input: AOCinput ) -> String {
+    let donut = parse( input: input )
+    return "\( donut.traverse() )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let donut = parse( input: input )
+    return "\( donut.traverseRecurse() )"
+}
+
+
+try runTestsPart1( part1: part1 )
+try runTestsPart2( part2: part2 )
+try runPart1( part1: part1 )
+try runPart2( part2: part2 )
