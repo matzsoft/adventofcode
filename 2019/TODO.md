7216f7a00fc82216bba699c0c1185aa460401f94
Mark Johnson <markj@matzsoft.com>
2021-06-22 13:43:47 -0700

    Converted 2019 day25 to use the library.

--- 2019/TODO.md
+++ 2019/TODO.md
@@ -4,4 +4,8 @@
 1. day 19 - compute commands in part 2 instead of hard coding them.
 1. day 21 - the solution essentially hard codes the answers.  There is support code in there to help determine what the hard coding should be.  day21a is a step in the direction of computing the required commands.  It would be nice to get that to work.
 1. day 22 - requires referencing a BigInt package from github.  Need a way to have that work through the adventOfCode tool.
-2. day 23 - part 2 takes 30+ seconds.  Can that be reduced?
+1. day 23 - part 2 takes 30+ seconds.  Can that be reduced?
+1. day 25 - solution is not general.  Need to develop a solution that works for any input. Possibilties from most satisfying to least satisfying:
+    1. A solution that plays the game, mapping the area and learns which are the safe items, then works out which of the safe items provide the corect weight.
+    1. A solution which interprets the input to get the information needed to correctly play the game.
+    1. A solution which extracts the answer directly from the input.
--- 2019/day25.swift
+++ 2019/day25.swift
@@ -1,209 +1,23 @@
 //
-//  main.swift
-//  day25
-//
-//  Created by Mark Johnson on 12/26/19.
-//  Copyright © 2019 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day25 - Cryostasis
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/22/21 12:36:05
 //
 
 import Foundation
 
-extension String: Error {}
-
-enum Opcode: Int {
-    case add                = 1
-    case multiply           = 2
-    case input              = 3
-    case output             = 4
-    case jumpIfTrue         = 5
-    case jumpIfFalse        = 6
-    case lessThan           = 7
-    case equals             = 8
-    case relativeBaseOffset = 9
-    case halt               = 99
-}
-
-enum ParameterMode: Int {
-    case position  = 0
-    case immediate = 1
-    case relative  = 2
-}
-
-struct Instruction {
-    let opcode: Opcode
-    let modes: [ParameterMode]
-
-    init( instruction: Int ) {
-        opcode = Opcode( rawValue: instruction % 100 )!
-        modes = [
-            ParameterMode( rawValue: instruction /   100 % 10 )!,
-            ParameterMode( rawValue: instruction /  1000 % 10 )!,
-            ParameterMode( rawValue: instruction / 10000 % 10 )!,
-        ]
-    }
-    
-    func mode( operand: Int ) -> ParameterMode {
-        return modes[ operand - 1 ]
-    }
-}
-
-class IntcodeComputer {
-    let name: String
-    var memory: [Int]
-    var inputs: [Int]
-    var pc = 0
-    var relativeBase = 0
-    var halted = true
-    var debug = false
-    
-    var nextInstruction: Instruction {
-        return Instruction( instruction: memory[pc] )
-    }
-    
-    init( name: String, memory: [Int], inputs: [Int] ) {
-        self.name = name
-        self.memory = memory
-        self.inputs = inputs
-    }
-
-    func fetch( _ instruction: Instruction, operand: Int ) throws -> Int {
-        var location = memory[ pc + operand ]
-        
-        switch instruction.mode( operand: operand ) {
-        case .position:
-            break
-        case .immediate:
-            return location
-        case .relative:
-            location += relativeBase
-        }
-
-        if location < 0 {
-            throw "Negative memory fetch (\(location)) at address \(pc)"
-        } else if location >= memory.count {
-            return 0
-        }
-        return memory[location]
-    }
-    
-    func store( _ instruction: Instruction, operand: Int, value: Int ) throws -> Void {
-        var location = memory[ pc + operand ]
-        
-        switch instruction.mode( operand: operand ) {
-        case .position:
-            break
-        case .immediate:
-            throw "Immediate mode invalid for address \(pc)"
-        case .relative:
-            location += relativeBase
-        }
-
-        if location < 0 {
-            throw "Negative memory store (\(location)) at address \(pc)"
-        } else if location >= memory.count {
-            memory.append( contentsOf: Array( repeating: 0, count: location - memory.count + 1 ) )
-        }
-        memory[location] = value
-    }
-    
-    func step() -> Int? {
-        let instruction = nextInstruction
-        
-        halted = false
-        switch instruction.opcode {
-        case .add:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-            
-            try! store( instruction, operand: 3, value: operand1 + operand2 )
-            pc += 4
-        case .multiply:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-
-            try! store( instruction, operand: 3, value: operand1 * operand2 )
-            pc += 4
-        case .input:
-            if debug { print( "\(name): inputs \(inputs.first!)" ) }
-            try! store( instruction, operand: 1, value: inputs.removeFirst() )
-            pc += 2
-        case .output:
-            let operand1 = try! fetch( instruction, operand: 1 )
-
-            pc += 2
-            if debug { print( "\(name): outputs \(operand1)" ) }
-            return operand1
-        case .jumpIfTrue:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-
-            if operand1 != 0 {
-                pc = operand2
-            } else {
-                pc += 3
-            }
-        case .jumpIfFalse:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-
-            if operand1 == 0 {
-                pc = operand2
-            } else {
-                pc += 3
-            }
-        case .lessThan:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-
-            if operand1 < operand2 {
-                try! store( instruction, operand: 3, value: 1 )
-            } else {
-                try! store( instruction, operand: 3, value: 0 )
-            }
-            pc += 4
-        case .equals:
-            let operand1 = try! fetch( instruction, operand: 1 )
-            let operand2 = try! fetch( instruction, operand: 2 )
-
-            if operand1 == operand2 {
-                try! store( instruction, operand: 3, value: 1 )
-            } else {
-                try! store( instruction, operand: 3, value: 0 )
-            }
-            pc += 4
-        case .relativeBaseOffset:
-            let operand1 = try! fetch( instruction, operand: 1 )
-
-            relativeBase += operand1
-            pc += 2
-        case .halt:
-            if debug { print( "\(name): halts" ) }
-            halted = true
-        }
-
-        return nil
-    }
-
-    func grind() -> Int? {
-        while true {
-            if let output = step() {
-                return output
-            }
-            
-            if halted {
-                return nil
-            }
-        }
-    }
-}
-
 struct Game {
-    let computer: IntcodeComputer
+    let computer: Intcode
     var inputQueue: [String] = []
     var outputQueue = ""
+    var lastOutput = ""
     
     init( memory: [Int] ) {
-        computer = IntcodeComputer( name: "AOC-Advent", memory: memory, inputs: [] )
+        computer = Intcode( name: "AOC-Advent", memory: memory )
     }
 
     func command( value: String ) -> Void {
@@ -211,7 +25,7 @@ struct Game {
         computer.inputs.append( Int( Character( "\n" ).asciiValue! ) )
     }
     
-    mutating func trial() -> Void {
+    mutating func trial() throws -> Void {
         var outputQueue = ""
         
         while true {
@@ -230,7 +44,7 @@ struct Game {
                 }
             }
 
-            if let output = computer.step() {
+            if let output = try computer.step() {
                 if let code = UnicodeScalar( output ) {
                     let char = Character( code )
                     
@@ -239,15 +53,14 @@ struct Game {
                             outputQueue.append( char )
                         } else {
                             print( outputQueue )
+                            lastOutput = outputQueue
                             outputQueue = ""
                         }
                     }
                 }
             }
             
-            if computer.halted {
-                break
-            }
+            if computer.nextInstruction.opcode == .halt { break }
         }
         
         print( outputQueue )
@@ -255,40 +68,54 @@ struct Game {
 }
 
 
-guard CommandLine.arguments.count > 1 else {
-    print( "No input file specified" )
-    exit( 1 )
+func parse( input: AOCinput ) -> Game {
+    let initialMemory = input.line.split( separator: "," ).map { Int( $0 )! }
+
+    return Game( memory: initialMemory )
 }
 
-let initialCommands = """
-south
-take mouse
-north
-west
-north
-north
-west
-take semiconductor
-east
-south
-west
-south
-take hypercube
-north
-east
-south
-west
-take antenna
-west
-south
-south
-south
-"""
-let input = try String( contentsOfFile: CommandLine.arguments[1] ).dropLast( 1 )
-let initialMemory = input.split( separator: "," ).map { Int($0)! }
-var game = Game( memory: initialMemory )
 
-game.inputQueue = initialCommands.split( separator: "\n" ).map { String( $0 ) }
-game.trial()
+func part1( input: AOCinput ) -> String {
+    var game = parse( input: input )
+    let initialCommands = """
+    south
+    take mouse
+    north
+    west
+    north
+    north
+    west
+    take semiconductor
+    east
+    south
+    west
+    south
+    take hypercube
+    north
+    east
+    south
+    west
+    take antenna
+    west
+    south
+    south
+    south
+    """
 
-//print( "Part 1: \(trial())" )
+    game.inputQueue = initialCommands.split( separator: "\n" ).map { String( $0 ) }
+    try! game.trial()
+    
+    let words = game.lastOutput.split( separator: " " )
+    return "\( words[11] )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    return "None"
+}
+
+
+try runTestsPart1( part1: part1 )
+try runTestsPart2( part2: part2 )
+try runPart1( part1: part1 )
+try runPart2( part2: part2 )
