ff537a06cf525f7840ce637aca0e40ce1747d99f
Mark Johnson <markj@matzsoft.com>
2021-06-23 17:24:37 -0700

    Converted 2020 day02 to use the library.

--- 2020/day02.swift
+++ 2020/day02.swift
@@ -1,38 +1,57 @@
 //
-//  main.swift
-//  day02
-//
-//  Created by Mark Johnson on 12/04/20.
-//  Copyright © 2020 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day02 - Password Philosophy
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/23/21 16:52:15
 //
 
 import Foundation
 
-let inputFile = "/Users/markj/Development/adventofcode/2020/input/day02.txt"
-let lines = try String( contentsOfFile: inputFile ).split( separator: "\n" )
-var oldValidCount = 0
-var newValidCount = 0
-
-for line in lines {
-    let fields   = line.split( whereSeparator: { "- :".contains( $0 ) } )
-    let least    = Int( fields[0] )!
-    let most     = Int( fields[1] )!
-    let letter   = Character( String( fields[2] ) )
-    let password = String( fields[3] )
-    let count    = password.reduce( 0 ) { $0 + ( $1 == letter ? 1 : 0 ) }
-
-    if least <= count && count <= most {
-        oldValidCount += 1
-    }
+struct Password {
+    let first: Int
+    let last: Int
+    let letter: Character
+    let value: [Character]
     
-    let index1 = password.index( password.startIndex, offsetBy: least - 1 )
-    let index2 = password.index( password.startIndex, offsetBy: most - 1 )
-    let condition1 = ( password[index1] == letter ) ? 1 : 0
-    let condition2 = ( password[index2] == letter ) ? 1 : 0
-    if ( condition1 ^ condition2 ) == 1 {
-        newValidCount += 1
+    init( line: String ) {
+        let fields = line.split( whereSeparator: { "- :".contains( $0 ) } )
+        
+        first  = Int( fields[0] )!
+        last   = Int( fields[1] )!
+        letter = Character( String( fields[2] ) )
+        value  = Array( fields[3] )
     }
 }
 
-print( "Part 1: \(oldValidCount)" )
-print( "Part 2: \(newValidCount)" )
+
+func parse( input: AOCinput ) -> [Password] {
+    return input.lines.map { Password( line: $0 ) }
+}
+
+
+func part1( input: AOCinput ) -> String {
+    let passwords = parse( input: input )
+    let counts = passwords.map { password in password.value.filter { $0 == password.letter }.count }
+    let pairs = zip( passwords, counts )
+    
+    return "\( pairs.filter { $0.0.first <= $0.1 && $0.1 <= $0.0.last }.count )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let passwords = parse( input: input )
+    let valid = passwords.filter {
+        ( $0.value[ $0.first - 1 ] == $0.letter ) != ( $0.value[ $0.last - 1 ] == $0.letter )
+    }
+    
+    return "\( valid.count )"
+}
+
+
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
