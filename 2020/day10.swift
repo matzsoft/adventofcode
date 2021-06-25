3071b14ca76d5520c850459989677c3c89937001
Mark Johnson <markj@matzsoft.com>
2021-06-25 13:02:39 -0700

    Converted 2020 day10 to use the library.

--- 2020/day10.swift
+++ 2020/day10.swift
@@ -1,13 +1,14 @@
 //
-//  main.swift
-//  day10
+//         FILE: main.swift
+//  DESCRIPTION: day10 - Adapter Array
+//        NOTES: Per the problem definition there can be no deltas with a value of zero.  However deltas
+//               with a value of two are not explicitly prohibited.  This is not a problem for Part 1, but
+//               my method for solving Part 2 will not work if there are any deltas with a value of 2.
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 06/25/21 12:35:48
 //
-//  Created by Mark Johnson on 12/09/20.
-//  Copyright © 2020 matzsoft. All rights reserved.
-//
-// Per the problem definition there can be no deltas with a value of zero.  However deltas with a value of
-// two are not explicitly prohibited.  This is not a problem for Part 1, but my method for solving Part 2
-// will not work if there are any deltas with a value of 2.
 
 import Foundation
 
@@ -22,21 +23,36 @@ func getFactor( count: Int ) -> Int {
 }
 
 
-let inputFile = "/Users/markj/Development/adventofcode/2020/input/day10.txt"
-let input = try String( contentsOfFile: inputFile ).split( separator: "\n" ).map { Int($0)! }.sorted()
-var last = 0
+func parse( input: AOCinput ) -> [Int] {
+    var last = 0
+    let adapters = input.lines.map { Int($0)! }.sorted()
 
-let deltas = input.map { (jolts) -> Int in let r = jolts - last; last = jolts; return r }
-let onesCount = deltas.filter { $0 == 1 }.count
-let twosCount = deltas.filter { $0 == 2 }.count
-let threesCount = deltas.filter { $0 == 3 }.count
-
-guard twosCount == 0 else {
-    print( "Unexpected delta(s) of 2 in input.  Unable to solve Part2." )
-    exit(0)
+    return adapters.map { (jolts) -> Int in let r = jolts - last; last = jolts; return r }
 }
 
-let runs = deltas.split( separator: 3 )
 
-print( "Part 1: \(onesCount * ( threesCount + 1 ))" )
-print( "Part 2: \(runs.reduce( 1 ) { $0 * getFactor( count: $1.count ) })" )
+func part1( input: AOCinput ) -> String {
+    let deltas = parse( input: input )
+    let onesCount = deltas.filter { $0 == 1 }.count
+    let threesCount = deltas.filter { $0 == 3 }.count
+
+    return "\( onesCount * ( threesCount + 1 ) )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let deltas = parse( input: input )
+    let twosCount = deltas.filter { $0 == 2 }.count
+    
+    guard twosCount == 0 else { return "Unexpected delta(s) of 2 in input.  Unable to solve Part2." }
+
+    let runs = deltas.split( separator: 3 )
+
+    return "\( runs.reduce( 1 ) { $0 * getFactor( count: $1.count ) } )"
+}
+
+
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
