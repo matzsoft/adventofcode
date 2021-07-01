987f6d989dc7c02a9b928027cf00fb905fc800f9
Mark Johnson <markj@matzsoft.com>
2021-07-01 16:14:22 -0700

    Converted 2020 day25 to use the library.

--- 2020/day25.swift
+++ 2020/day25.swift
@@ -1,9 +1,11 @@
 //
-//  main.swift
-//  day25
-//
-//  Created by Mark Johnson on 01/01/21.
-//  Copyright © 2021 matzsoft. All rights reserved.
+//         FILE: main.swift
+//  DESCRIPTION: day25 - Combo Breaker
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 07/01/21 16:00:07
 //
 
 import Foundation
@@ -19,7 +21,7 @@ struct Device {
         return value * subject % modulus
     }
     
-    init( input: Substring ) {
+    init( input: String ) {
         var value = startValue
         var loopCount = 0
 
@@ -46,10 +48,30 @@ struct Device {
 }
 
 
-let inputFile = "/Users/markj/Development/adventofcode/2020/input/day25.txt"
-let devices = try String( contentsOfFile: inputFile ).split( separator: "\n" ).map { Device( input: $0 ) }
+func parse( input: AOCinput ) -> [Device] {
+    return input.lines.map { Device( input: $0 ) }
+}
 
 
-print( "Part 1: \( devices[0].fullTransform( subject: devices[1].publicKey ) )" )
-print( "Part 1: \( devices[1].fullTransform( subject: devices[0].publicKey ) )" )
-//print( "Part 2: \( part2( initial: blacks ) )" )
+func part1( input: AOCinput ) -> String {
+    let devices = parse( input: input )
+    let card = devices[1].fullTransform( subject: devices[0].publicKey )
+    let door = devices[0].fullTransform( subject: devices[1].publicKey )
+    
+    guard card == door else {
+        return "Card and door don't match."
+    }
+    
+    return "\( card )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    return "None"
+}
+
+
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
