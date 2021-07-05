952fc8158ca126596e4dcc2f986bfbedc3585e4d
Mark Johnson <markj@matzsoft.com>
2021-07-04 22:54:42 -0700

    Reimplemented 2015 day04 in Swift using library.
    Also added md5Fast to Crypto.swift.  It runs about 20 times faster than
    md5Hash because it returns the raw bytes instead of converting them to a
    hexidecimal string..

--- tools/adventOfCode/Sources/adventOfCode/Library/Crypto.swift
+++ tools/adventOfCode/Sources/adventOfCode/Library/Crypto.swift
@@ -18,3 +18,14 @@ func md5Hash( str: String ) -> String {
 
     return digest.map { String( format: "%02x", UInt8( $0 ) ) }.joined()
 }
+
+
+func md5Fast( str: String ) -> [UInt8] {
+    guard let string = str.data( using: .utf8 ) else { return [] }
+    
+    var digest = [UInt8]( repeating: 0, count: Int( CC_MD5_DIGEST_LENGTH ) )
+    
+    _ = string.withUnsafeBytes { CC_MD5( $0.baseAddress, UInt32( string.count ), &digest ) }
+
+    return digest
+}
