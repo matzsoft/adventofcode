726b30be52284a98caf5fe5f3d2068c0415b5e39
Mark Johnson <markj@matzsoft.com>
2021-08-13 11:12:27 -0700

    Added trace capability to the IntCode interpreter.

--- tools/adventOfCode/Sources/adventOfCode/Library/Intcode.swift
+++ tools/adventOfCode/Sources/adventOfCode/Library/Intcode.swift
@@ -158,4 +158,163 @@ class Intcode {
             if nextInstruction.opcode == .halt { return nil }
         }
     }
+
+    func operandDescription( _ instruction: Instruction, operand: Int ) -> String {
+        let location = memory[ ip + operand ]
+        
+        switch instruction.mode( operand: operand ) {
+        case .position:
+            return "@\(location)"
+        case .immediate:
+            return "\(location)"
+        case .relative:
+            return "*\(location)"
+        }
+    }
+    
+    func storeLocation( _ instruction: Instruction, operand: Int ) throws -> Int {
+        var location = memory[ ip + operand ]
+        
+        switch instruction.mode( operand: operand ) {
+        case .position:
+            break
+        case .immediate:
+            throw RuntimeError( "Immediate mode invalid for address \(ip)" )
+        case .relative:
+            location += relativeBase
+        }
+
+        if location < 0 {
+            throw RuntimeError( "Negative memory store (\(location)) at address \(ip)" )
+        }
+        
+        return location
+    }
+
+    func trace() throws -> String {
+        let instruction = Instruction( instruction: memory[ip] )
+        var line = String( format: "%04d: ", ip )
+        
+        func pad() -> Void {
+            let column = 35
+            guard line.count < column else { return }
+            line.append( String( repeating: " ", count: column - line.count ) )
+        }
+        
+        switch instruction.opcode {
+        case .add:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "add " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( "\(operandDescription( instruction, operand: 2 ) ), " )
+            line.append( operandDescription( instruction, operand: 3 ) )
+            pad()
+            line.append( "\(operand1) + \(operand2) = \(operand1 + operand2) -> " )
+            line.append( "\( try storeLocation( instruction, operand: 3 ) )" )
+        case .multiply:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "multiply " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( "\(operandDescription( instruction, operand: 2 ) ), " )
+            line.append( operandDescription( instruction, operand: 3 ) )
+            pad()
+            line.append( "\(operand1) * \(operand2) = \(operand1 * operand2) -> " )
+            line.append( "\( try storeLocation( instruction, operand: 3 ) )" )
+        case .input:
+            let value = inputs.first!
+            line.append( "input " )
+            line.append( operandDescription( instruction, operand: 1 ) )
+            pad()
+            line.append( "input \(value)" )
+            if 0 < value && value < 256 {
+                if let code = UnicodeScalar( value ) {
+                    let char = Character( code )
+                    
+                    if char.isASCII {
+                        if char != "\n" {
+                            line.append( " \"\(char)\"" )
+                        } else {
+                            line.append( " \"\\n\"" )
+                        }
+                    }
+                }
+            }
+            line.append( " -> \( try storeLocation( instruction, operand: 1 ) )" )
+        case .output:
+            let operand1 = try fetch( instruction, operand: 1 )
+            
+            line.append( "output " )
+            line.append( operandDescription( instruction, operand: 1 ) )
+            pad()
+            line.append( "output \(operand1)" )
+            if 0 < operand1 && operand1 < 256 {
+                if let code = UnicodeScalar( operand1 ) {
+                    let char = Character( code )
+                    
+                    if char.isASCII {
+                        if char != "\n" {
+                            line.append( " \"\(char)\"" )
+                        } else {
+                            line.append( " \"\\n\"" )
+                        }
+                    }
+                }
+            }
+        case .jumpIfTrue:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "jumpIfTrue " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( operandDescription( instruction, operand: 2 ) )
+            pad()
+            line.append( "jumpIfTrue \(operand1), \(operand2)" )
+        case .jumpIfFalse:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "jumpIfFalse " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( operandDescription( instruction, operand: 2 ) )
+            pad()
+            line.append( "jumpIfFalse \(operand1), \(operand2)" )
+        case .lessThan:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "lessThan " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( "\(operandDescription( instruction, operand: 2 ) ), " )
+            line.append( operandDescription( instruction, operand: 3 ) )
+            pad()
+            line.append( "\(operand1) < \(operand2) => \( operand1 < operand2 ? 1 : 0 ) -> " )
+            line.append( "\( try storeLocation( instruction, operand: 3 ) )" )
+        case .equals:
+            let operand1 = try fetch( instruction, operand: 1 )
+            let operand2 = try fetch( instruction, operand: 2 )
+            
+            line.append( "equals " )
+            line.append( "\(operandDescription( instruction, operand: 1 ) ), " )
+            line.append( "\(operandDescription( instruction, operand: 2 ) ), " )
+            line.append( operandDescription( instruction, operand: 3 ) )
+            pad()
+            line.append( "\(operand1) == \(operand2) => \( operand1 == operand2 ? 1 : 0 ) -> " )
+            line.append( "\( try storeLocation( instruction, operand: 3 ) )" )
+        case .relativeBaseOffset:
+            let operand1 = try fetch( instruction, operand: 1 )
+            
+            line.append( "relativeBaseOffset " )
+            line.append( operandDescription( instruction, operand: 1 ) )
+            pad()
+            line.append( "relativeBase = \( relativeBase + operand1 )" )
+        case .halt:
+            line.append( "halt" )
+        }
+
+        return line
+    }
 }
