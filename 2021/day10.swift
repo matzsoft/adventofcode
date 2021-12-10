9b9f77ccff92c37be6b0872a27be1dc309b11d28
Mark Johnson <markj@matzsoft.com>
2021-12-10 12:58:47 -0800

    2021 day10 cleanup.

--- 2021/day10.swift
+++ 2021/day10.swift
@@ -14,75 +14,60 @@ struct Pair {
     let open: Character
     let close: Character
     let corruptScore: Int
-    let incompleteScore: Int
+    let autoCompleteScore: Int
 }
 
 let pairs = [
-    Pair( open: "(", close: ")", corruptScore: 3, incompleteScore: 1 ),
-    Pair( open: "[", close: "]", corruptScore: 57, incompleteScore: 2 ),
-    Pair( open: "{", close: "}", corruptScore: 1197, incompleteScore: 3 ),
-    Pair( open: "<", close: ">", corruptScore: 25137, incompleteScore: 4 ),
+    Pair( open: "(", close: ")", corruptScore: 3,     autoCompleteScore: 1 ),
+    Pair( open: "[", close: "]", corruptScore: 57,    autoCompleteScore: 2 ),
+    Pair( open: "{", close: "}", corruptScore: 1197,  autoCompleteScore: 3 ),
+    Pair( open: "<", close: ">", corruptScore: 25137, autoCompleteScore: 4 ),
 ]
-let close2open = pairs.reduce(into: [ Character : Pair ]() ) { $0[$1.close] = $1 }
-let open2close = pairs.reduce(into: [ Character : Pair ]() ) { $0[$1.open] = $1 }
+let close2open = pairs.reduce( into: [ Character : Pair ]() ) { $0[$1.close] = $1 }
+let open2close = pairs.reduce( into: [ Character : Pair ]() ) { $0[$1.open]  = $1 }
 
+func corruptScore( line: String ) -> Int {
+    var stack = [ Character ]()
+    
+    for character in line {
+        if let closing = close2open[character] {
+            if stack.isEmpty || stack.removeLast() != closing.open {
+                return closing.corruptScore
+            }
+        } else {
+            stack.append( character )
+        }
+    }
+    
+    return 0
+}
 
-func parse( input: AOCinput ) -> [[Character]] {
-    return input.lines.map { Array( $0 ) }
+func autoCompleteScore( line: String ) -> Int? {
+    var stack = [ Character ]()
+    
+    for character in line {
+        if let closing = close2open[character] {
+            if stack.isEmpty || stack.removeLast() != closing.open {
+                return nil
+            }
+        } else {
+            stack.append( character )
+        }
+    }
+    if stack.isEmpty { return nil }
+    
+    return stack.reversed().map { open2close[$0]! }.reduce( 0 ) { $0 * 5 + $1.autoCompleteScore }
 }
 
 
 func part1( input: AOCinput ) -> String {
-    let lines = parse( input: input )
-    var score = 0
-    
-    LINE:
-    for line in lines {
-        var stack = [ Character ]()
-        
-        for character in line {
-            if let closing = close2open[character] {
-                if stack.isEmpty || stack.last! != closing.open {
-                    score += closing.corruptScore
-                    continue LINE
-                } else {
-                    stack.removeLast()
-                }
-            } else {
-                stack.append( character )
-            }
-        }
-    }
-    return "\(score)"
+    return "\( input.lines.reduce( 0 ) { $0 + corruptScore( line: $1 ) } )"
 }
 
 
 func part2( input: AOCinput ) -> String {
-    let lines = parse( input: input )
-    var scores = [Int]()
+    let scores = input.lines.compactMap { autoCompleteScore( line: $0 ) }.sorted()
     
-    LINE:
-    for line in lines {
-        var stack = [ Character ]()
-        
-        for character in line {
-            if let closing = close2open[character] {
-                if stack.isEmpty || stack.last! != closing.open {
-                    continue LINE
-                } else {
-                    stack.removeLast()
-                }
-            } else {
-                stack.append( character )
-            }
-        }
-        if !stack.isEmpty {
-            scores.append( stack.reversed().map { open2close[$0]! }.reduce( 0 ) {
-                $0 * 5 + $1.incompleteScore }
-            )
-        }
-    }
-    scores.sort()
     if scores.count.isMultiple( of: 2 ) { return "No solution" }
     return "\( scores[ scores.count / 2 ] )"
 }
