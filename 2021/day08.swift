c4b2d9d17b9b8265fc00ff8c5dda38450a457135
Mark Johnson <markj@matzsoft.com>
2021-12-11 12:57:46 -0800

    2021 day08 cleanup.

--- 2021/day08.swift
+++ 2021/day08.swift
@@ -10,6 +10,14 @@
 
 import Foundation
 
+let correct = [
+    Set( "abcefg" ), Set( "cf" ), Set( "acdeg" ), Set( "acdfg" ), Set( "bcdf" ),
+    Set( "abdfg" ), Set( "abdefg" ), Set( "acf" ), Set( "abcdefg" ), Set( "abcdfg" )
+]
+let uniqueCounts = correct.reduce( into: [ Int : Int ]() ) {
+    $0[ $1.count, default: 0 ] += 1
+}.filter { $0.value == 1 }.map { $0.key }
+
 struct Note {
     let input: [Set<Character>]
     let output: [Set<Character>]
@@ -17,7 +25,7 @@ struct Note {
     init( line: String ) {
         let words = line.split(whereSeparator: { " |".contains( $0 ) } )
         
-        input = words[0..<10].map { Set( $0 ) }
+        input = words[..<10].map { Set( $0 ) }
         output = words[10...].map { Set( $0 ) }
     }
 }
@@ -34,29 +42,25 @@ func parse( input: AOCinput ) -> [Note] {
 
 
 func part1( input: AOCinput ) -> String {
-    let acceptable = Set( [ 2, 3, 4, 7 ] )
     let notes = parse( input: input )
-    let digits = notes.flatMap { $0.output }.filter { acceptable.contains( $0.count ) }
+    let digits = notes.flatMap { $0.output }.filter { uniqueCounts.contains( $0.count ) }
     return "\(digits.count)"
 }
 
 
 func part2( input: AOCinput ) -> String {
     let notes = parse( input: input )
-    let easy = [ 1, 4, 7, 8 ]
-    let correct = [
-        Set( "abcefg" ), Set( "cf" ), Set( "acdeg" ), Set( "acdfg" ), Set( "bcdf" ),
-        Set( "abdfg" ), Set( "abdefg" ), Set( "acf" ), Set( "abcdefg" ), Set( "abcdfg" )
-    ]
-    let uniques = easy.map { correct[$0] }    
+    let uniqueDigits = uniqueCounts.map { count in correct.first( where: { $0.count == count } )! }
     let signatures = ( 0 ..< correct.count ).reduce( into: [ Int : Int ]() ) {
-        $0[ signature( digit: correct[$1], uniques: uniques ) ] = $1
+        $0[ signature( digit: correct[$1], uniques: uniqueDigits ) ] = $1
     }
     var sum = 0
     
     for note in notes {
-        let peasy = easy.map { digit in note.input.first( where: { $0.count == correct[digit].count } )! }
-        let value = note.output.map { String( signatures[ signature( digit: $0, uniques: peasy ) ]! ) }
+        let uniqueInputs = uniqueCounts.map { count in note.input.first( where: { $0.count == count } )! }
+        let value = note.output.map {
+            String( signatures[ signature( digit: $0, uniques: uniqueInputs ) ]! )
+        }
         
         sum += Int( value.joined() )!
     }
