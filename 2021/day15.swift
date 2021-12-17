650c6fc2733440ca9368d8a91e60e74ce38e83f5
Mark Johnson <markj@matzsoft.com>
2021-12-17 12:45:49 -0800

    Added 2021 day15 (part 1 plus starting part 2).

---
+++ 2021/day15.swift
@@ -0,0 +1,151 @@
+//
+//         FILE: main.swift
+//  DESCRIPTION: day15 - Chiton
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2021 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 12/14/21 23:01:30
+//
+
+import Foundation
+
+struct Map {
+    let map: [[Int]]
+    let bounds: Rect2D
+    let end: Point2D
+    
+    subscript( position: Point2D ) -> Int { return map[position.y][position.x] }
+    
+    var points: [Point2D] {
+        ( 0 ..< map.count ).map {
+            row in ( 0 ..< map[0].count ).map { col in Point2D( x: col, y: row ) }
+        }.flatMap { $0 }
+    }
+    
+    init( lines: [String] ) {
+        map = lines.map { $0.map { Int( String( $0 ) )! } }
+        end = Point2D( x: map[0].count - 1, y: map.count - 1 )
+        bounds = Rect2D(min: Point2D( x: 0, y: 0 ), max: end )
+    }
+    
+    init( expand: Map, by: Int ) {
+        map = expand.map.map { $0.map { risk -> Int in
+            let new = risk + by
+            return new < 10 ? new : new - 9 }
+        }
+        end = expand.end
+        bounds = expand.bounds
+    }
+}
+
+struct RiskMatrix {
+    let map: Map
+    var risks: [[Int]]
+    var vertexSet: Set<Point2D>
+    
+    subscript( position: Point2D ) -> Int {
+        get { return risks[position.y][position.x] }
+        set( newValue ) { risks[position.y][position.x] = newValue }
+    }
+
+    init( map: Map, start: Point2D ) {
+        self.map = map
+        risks = Array(
+            repeating: Array( repeating: Int.max, count: map.bounds.width ),
+            count: map.bounds.height
+        )
+        
+        vertexSet = Set( map.points )
+        self[ start ] = 0
+    }
+    
+    mutating func scan( predicate: ( Point2D ) -> Bool ) -> Point2D? {
+        while !vertexSet.isEmpty {
+            let position = vertexSet.min( by: { self[$0] < self[$1] } )!
+            
+            vertexSet.remove( position )
+            if predicate( position ) { return position }
+
+            for neighbor in DirectionUDLR.allCases
+                    .map( { position + $0.vector } )
+                    .filter( { vertexSet.contains( $0 ) } )
+            {
+                let risk = self[position] + map[neighbor]
+                
+                if risk < self[neighbor] {
+                    self[neighbor] = risk
+                }
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct TiledMap {
+    let maps: [Map]
+    
+    subscript( position: Point2D ) -> Map { return maps[ position.y + position.x ] }
+    
+    var bounds: Rect2D { Rect2D( min: Point2D( x: 0, y: 0 ), width: 5, height: 5 )! }
+    
+    init( initial: Map ) {
+        maps = [ initial ] + ( 1 ... 8 ).map { Map( expand: initial, by: $0 ) }
+    }
+}
+
+
+//1  function Dijkstra(Graph, source):
+//2
+//3      create vertex set Q
+//4
+//5      for each vertex v in Graph:
+//6          dist[v] ← INFINITY
+//7          prev[v] ← UNDEFINED
+//8          add v to Q
+//9      dist[source] ← 0
+//10
+//11      while Q is not empty:
+//12          u ← vertex in Q with min dist[u]
+//13
+//14          remove u from Q
+//15
+//16          for each neighbor v of u still in Q:
+//17              alt ← dist[u] + length(u, v)
+//18              if alt < dist[v]:
+//19                  dist[v] ← alt
+//20                  prev[v] ← u
+//21
+//22      return dist[], prev[]
+
+
+func parse( input: AOCinput ) -> Map {
+    return Map( lines: input.lines )
+}
+
+
+func part1( input: AOCinput ) -> String {
+    let map = parse( input: input )
+    var risks = RiskMatrix( map: map, start: Point2D( x: 0, y: 0 ) )
+
+    if let ending = risks.scan( predicate: { $0 == map.end } ) {
+        return "\( risks[ending] )"
+    }
+    return "No solution"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let map = parse( input: input )
+    let expanded = Map( expand: map, by: 1 )
+    
+    for row in expanded.map { print( row.map { String( $0 ) }.joined() ) }
+    return ""
+}
+
+
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
