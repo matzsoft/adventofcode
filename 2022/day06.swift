96f0b29774e0a2a2401cfa45d2163c15b34f62ad
Mark Johnson <markj@matzsoft.com>
2022-12-05 21:56:54 -0800

    Added 2022 day06.

---
+++ 2022/day06.swift
@@ -0,0 +1,39 @@
+//
+//         FILE: main.swift
+//  DESCRIPTION: day06 - Tuning Trouble
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2022 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 12/05/22 21:06:04
+//
+
+import Foundation
+
+func solution( input: AOCinput, length: Int ) -> String {
+    let characters = Array( input.line )
+    
+    for index in 0 ..< characters.count - length + 1 {
+        let set = Set( characters[ index ... index + length - 1 ] )
+        if set.count == length { return "\( index + length )" }
+    }
+    
+    return "No solution"
+}
+
+
+func part1( input: AOCinput ) -> String {
+    return "\( solution( input: input, length: 4 ) )"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    return "\( solution( input: input, length: 14 ) )"
+}
+
+
+try print( projectInfo() )
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
