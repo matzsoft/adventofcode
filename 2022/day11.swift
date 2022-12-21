2e10e91fbe729c0fe2644da45fe8ba6b848fca8d
Mark Johnson <markj@matzsoft.com>
2022-12-20 23:10:27 -0800

    Got 2022 day11 part2 working.  Cleanup needed.

--- 2022/day11.swift
+++ 2022/day11.swift
@@ -10,17 +10,18 @@
 
 import Foundation
 
-let primes = Primes( to: 10000 )
+let primeLimit = 1000
+let primes = Primes( to: primeLimit )
 
 struct PFInt {
-    let factors: [ Int : Int ]
+    let factors: Set<Int>
     
-    init( factors: [ Int : Int ] ) {
+    init( factors: Set<Int> ) {
         self.factors = factors
     }
     
     init( _ value: Int ) {
-        factors = Dictionary( uniqueKeysWithValues: primes.primeFactors( of: value ) )
+        factors = Set( primes.primeFactors( of: value ).map { $0.0 } )
     }
     
     init?( _ value: String.SubSequence ) {
@@ -28,57 +29,47 @@ struct PFInt {
         self.init( value )
     }
     
-    var toInt: Int {
-        factors.keys.reduce( 1, * )
-    }
+//    var toInt: Int {
+//        factors.keys.reduce( 1, * )
+//    }
     
     static func *( left: PFInt, right: PFInt ) -> PFInt {
-        var factors = left.factors
-        for factor in right.factors {
-            factors[ factor.key, default: 0 ] += factor.value
-        }
-        return PFInt( factors: factors )
+        return PFInt( factors: left.factors.union( right.factors ) )
     }
     
     static func +( left: PFInt, right: PFInt ) -> PFInt {
-        return PFInt( left.toInt + right.toInt )
+        return PFInt( factors: left.factors.intersection( right.factors ) )
 //        guard let quotient = left / right else { fatalError( "Trying to add a non-factor" ) }
 //        guard right.factors.count == 1 else { fatalError( "Unable to add non primes" ) }
 //        return quotient
     }
     
-    static func /( left: PFInt, right: PFInt ) -> PFInt? {
-        var factors = left.factors
-        for factor in right.factors {
-            if factors[ factor.key, default: 0 ] < factor.value { return nil }
-            factors[ factor.key, default: factor.value ] -= factor.value
-        }
-        return PFInt( factors: factors )
-    }
+//    static func /( left: PFInt, right: PFInt ) -> PFInt? {
+//        var factors = left.factors
+//        for factor in right.factors {
+//            if factors[ factor.key, default: 0 ] < factor.value { return nil }
+//            factors[ factor.key, default: factor.value ] -= factor.value
+//        }
+//        return PFInt( factors: factors )
+//    }
     
     func doubled() -> PFInt {
-        var factors = factors
-        
-        factors[ 2, default: 0 ] += 1
-        return PFInt( factors: factors )
+        return PFInt( factors: factors.union( Set( [ 2 ] ) ) )
     }
     
     func squared() -> PFInt {
-        PFInt( factors: factors.mapValues { $0 * 2 } )
+        return self
     }
     
     func isMultiple( of: Int ) -> Bool {
-        factors[of] != nil
-    }
-    
-    func knockBack() -> PFInt {
-        PFInt( factors: factors.mapValues { _ in 1 } )
+        factors.contains( of )
     }
 }
 
 class Simian {
     var items: [PFInt]
     var inspectionCount = 0
+    let divisor: Int
     let operation: (PFInt) -> PFInt
     let test: (PFInt) -> Bool
     let ifTrue: Int
@@ -109,6 +100,7 @@ class Simian {
         
         ifTrue = Int( lines[4].split( separator: " " )[5] )!
         ifFalse = Int( lines[5].split( separator: " " )[5] )!
+        self.divisor = divisor
     }
     
     func turn( monkeys: [Simian] ) -> Void {
@@ -120,6 +112,89 @@ class Simian {
         items = []
     }
 }
+
+
+struct Worry {
+    
+    let value: Int
+    let modulus: Int
+    
+    internal init( value: Int, modulus: Int ) {
+        self.value = value
+        self.modulus = modulus
+    }
+
+//    init( value: Int, factors: Set<Int> ) {
+//        self.value = value
+//        self.factors = factors
+//    }
+    
+//    var squared: Worry {
+//        if value < primeLimit { return Worry( value: value * value ) }
+//        return self
+//    }
+    
+    static func *( left: Worry, right: Worry ) -> Worry {
+        Worry( value: ( left.value * right.value ) % left.modulus, modulus: left.modulus )
+    }
+    
+    static func +( left: Worry, right: Worry ) -> Worry {
+        Worry( value: ( left.value + right.value ) % left.modulus, modulus: left.modulus )
+    }
+    
+    func isMultiple( of: Int ) -> Bool {
+        value.isMultiple( of: of )
+    }
+}
+
+
+class Primate {
+    var items: [Worry]
+    var inspectionCount = 0
+    let operation: (Worry) -> Worry
+    let test: (Worry) -> Bool
+    let ifTrue: Int
+    let ifFalse: Int
+    let bigN: Int
+    
+    init( lines: [String], bigN: Int ) {
+        let items = lines[1].split( whereSeparator: { " ,".contains( $0 ) } )
+        self.items = items.dropFirst( 2 ).map { Worry( value: Int( $0 )!, modulus: bigN ) }
+
+        let operation = lines[2].split( separator: " " )
+        if let factor = Int( operation[5] ) {
+            let worry = Worry( value: factor, modulus: bigN )
+            if operation[4] == "*" {
+                self.operation = { $0 * worry }
+            } else {
+                self.operation = { $0 + worry }
+            }
+        } else {
+            if operation[4] == "*" {
+                self.operation = { $0 * $0 }
+            } else {
+                self.operation = { $0 + $0 }
+            }
+        }
+
+        let divisor = Int( lines[3].split( separator: " " )[3] )!
+        self.test = { $0.isMultiple( of: divisor ) }
+        
+        ifTrue = Int( lines[4].split( separator: " " )[5] )!
+        ifFalse = Int( lines[5].split( separator: " " )[5] )!
+        self.bigN = bigN
+    }
+    
+    func turn( monkeys: [Primate] ) -> Void {
+        inspectionCount += items.count
+        for item in items {
+            let worry = operation( item )
+            monkeys[ test( worry ) ? ifTrue : ifFalse ].items.append( worry )
+        }
+        items = []
+    }
+}
+
 
 class Monkey {
     var items: [Int]
@@ -178,9 +253,15 @@ func part1( input: AOCinput ) -> String {
     return "\( counts[0] * counts[1] )"
 }
 
+func extractor( input: AOCinput ) -> Int {
+    let divisors = input.paragraphs.map { Int( $0[3].split( separator: " " )[3] )! }
+    return Set( divisors ).sorted().reduce( 1, * )
+}
+
 
 func part2( input: AOCinput ) -> String {
-    let monkeys = input.paragraphs.map { Simian( lines: $0 ) }
+    let bigN = extractor( input: input )
+    let monkeys = input.paragraphs.map { Primate( lines: $0, bigN: bigN ) }
 
     for _ in 1 ... 10000 {
         monkeys.forEach { $0.turn( monkeys: monkeys ) }
