4315b623a9257a50804de647ecb39e9b3302a17b
Mark Johnson <markj@matzsoft.com>
2023-04-10 13:50:16 -0700

    Library - cleaned up Coprocessor.swift.
    1. Added a public attribute and grouped them together.
    2. Simplified the code of most of the opcode functions.
    3. Added documentation throughout.

--- tools/Library/Sources/Coprocessor.swift
+++ tools/Library/Sources/Coprocessor.swift
@@ -7,15 +7,26 @@
 
 import Foundation
 
+/// Implements the Coprocessor computer used by 2017 days 18 and 23.
+///
+/// In addition to basic emulator functionality the following features are provided:
+/// - Multiple breakpoints with an action routine associated with each
+/// - Cycle tracing
+/// - ip tracing
+/// - A dump facility that allows printing the contents of memory, nicely formatted.
 public class Coprocessor {
     public enum Opcode: String, CaseIterable { case snd, set, add, sub, mul, mod, rcv, jgz, jnz }
     enum State { case running, waiting, halted }
 
+    /// Used to hold the instructions stored in memory.
     public class Instruction {
         public var mnemonic: Opcode
-        let x: String
+        public let x: String
         public let y: String
         
+        /// Initialize an instruction from assembly code.
+        /// - Parameter input: A String consisting of opcode, x, and y seperated by space.
+        /// The values of x and y ahould be either integer or a register designation.
         init( input: String ) {
             let instruction = input.split( separator: " " )
             
@@ -34,17 +45,11 @@ public class Coprocessor {
         }
     }
 
-    struct Trace {
-        var lastHit: Int?
-        var lastCycle: Int?
-        var cycleCount: Int
-    }
-
+    public var memory: [Instruction]
     var registers = [ String : Int ]()
     var ip = 0
     var cycleNumber = 0
     var opcodes: [ Opcode : ( String, String ) -> Void ]
-    public var memory: [Instruction]
     var queue: [Int]
     var state: State
     var receiver: Coprocessor?
@@ -55,6 +60,9 @@ public class Coprocessor {
     var ipTraceStop = Int.max
     var breakPoints = [ Int : ( ( Coprocessor ) -> Bool ) ]()
     
+    /// Initialize a Coprocessor from assembly code.
+    /// - Parameter lines: An array of lines.  Each line should have an opcode, x, and y seperated by space.
+    /// The values of x and y ahould be either integer or a register designation.
     public init( lines: [String] ) {
         memory = lines.map { Instruction( input: $0 ) }
         
@@ -75,86 +83,78 @@ public class Coprocessor {
         ]
     }
     
+    /// Implements the action for the snd opcode.
+    ///
+    /// Sends the value of x to the receiver (if defined).
+    /// - Parameters:
+    ///   - x: Either an integer which means send that value or a register designation
+    ///   which means send the value of that register.
+    ///   - y: Ignored.
     func send( x: String, y: String ) -> Void {
-        if let value = Int( x ) {
-            receiver?.receive( message: value )
-        } else if let value = registers[x] {
-            receiver?.receive( message: value )
-        } else {
-            receiver?.receive( message: 0 )
-        }
+        receiver?.receive( message: ( Int( x ) ?? ( registers[x] ?? 0 ) ) )
     }
     
+    /// Implements the action for the set opcode.
+    ///
+    /// Store the value denoted by y into the register designated by x.
+    /// - Parameters:
+    ///   - x: The register to set to the value.
+    ///   - y: The value to set into the register.
+    ///   Either a literal integer or the designation of the register containing value.
     func set( x: String, y: String ) -> Void {
-        if let value = Int( y ) {
-            registers[x] = value
-        } else if let value = registers[y] {
-            registers[x] = value
-        } else {
-            registers[x] = 0
-        }
+        registers[x] = Int( y ) ?? ( registers[y] ?? 0 )
     }
     
+    /// Implements the action for the add opcode.
+    ///
+    /// Store the sum of register x and the value denoted by y into the register designated by x.
+    /// - Parameters:
+    ///   - x: The register used to get the first addend and to store the sum.
+    ///   - y: The value for the second addend.
     func add( x: String, y: String ) -> Void {
-        var addend2 = 0
-        
-        if let value = Int( y ) {
-            addend2 = value
-        } else if let value = registers[y] {
-            addend2 = value
-        }
-        
-        if let addend1 = registers[x] {
-            registers[x] = addend1 + addend2
-        } else {
-            registers[x] = addend2
-        }
+        registers[ x, default: 0 ] += Int( y ) ?? ( registers[y] ?? 0 )
     }
     
+    /// Implements the action for the sub opcode.
+    ///
+    /// Store the difference of register x and the value denoted by y into the register designated by x.
+    /// - Parameters:
+    ///   - x: The register used to get the minuend and to store the difference.
+    ///   - y: The value for the subtrahend.
     func sub( x: String, y: String ) -> Void {
-        var addend2 = 0
-        
-        if let value = Int( y ) {
-            addend2 = value
-        } else if let value = registers[y] {
-            addend2 = value
-        }
-        
-        if let addend1 = registers[x] {
-            registers[x] = addend1 - addend2
-        } else {
-            registers[x] = -addend2
-        }
+        registers[ x, default: 0 ] -= Int( y ) ?? ( registers[y] ?? 0 )
     }
     
+    /// Implements the action for the mul opcode.
+    ///
+    /// Store the product of register x and the value denoted by y into the register designated by x.
+    /// - Parameters:
+    ///   - x: The register used to get the multiplicand and to store the product.
+    ///   - y: The value for the multiplier.
     func multiply( x: String, y: String ) -> Void {
-        var multiplier = 0
-        
-        if let value = Int( y ) {
-            multiplier = value
-        } else if let value = registers[y] {
-            multiplier = value
-        }
-        
-        if let multiplicand = registers[x] {
-            registers[x] = multiplicand * multiplier
-        }
+        registers[ x, default: 0 ] *= Int( y ) ?? ( registers[y] ?? 0 )
     }
     
+    /// Implements the action for the mod opcode.
+    ///
+    /// Store the remainder of register x divided by the value denoted by y
+    /// into the register designated by x.
+    /// - Parameters:
+    ///   - x: The register used to get the dividend and to store the remainder.
+    ///   - y: The value for the divisor.
     func modulo( x: String, y: String ) -> Void {
-        var divisor = 0
-        
-        if let value = Int( y ) {
-            divisor = value
-        } else if let value = registers[y] {
-            divisor = value
-        }
-        
-        if let dividend = registers[x] {
-            registers[x] = dividend % divisor
-        }
+        registers[ x, default: 0 ] %= Int( y ) ?? ( registers[y] ?? 0 )
     }
     
+    /// Implements the action for the rcv opcode.
+    ///
+    /// Remove the first entry in the input queue and store it into the register designated by x.
+    ///
+    /// If the input queue is empty, enter the waiting state and modify the ip so that
+    /// the rcv instruction will be executed when the next value is entere into the input queue.
+    /// - Parameters:
+    ///   - x: The register used to store the received value.
+    ///   - y: Ignored.
     func receive( x: String, y: String ) -> Void {
         if let regValue = registers[x], regValue != 0 {
             if let value = queue.first {
@@ -168,48 +168,31 @@ public class Coprocessor {
         }
     }
 
+    /// Implements the action for the jgz opcode.
+    ///
+    /// Relative jump by the offset of the value of y if the value of x is greater than zero.
+    /// - Parameters:
+    ///   - x: The value to test for greater than zero.
+    ///   - y: The relative jump offset if x > 0.
     func jumpGTZero( x: String, y: String ) -> Void {
-        var xval = 0
-        var yval = 0
-        
-        if let value = Int( x ) {
-            xval = value
-        } else if let value = registers[x] {
-            xval = value
-        }
-        
-        if let value = Int( y ) {
-            yval = value
-        } else if let value = registers[y] {
-            yval = value
-        }
-        
-        if xval > 0 {
-            ip += yval - 1
+        if ( Int( x ) ?? ( registers[x] ?? 0 ) ) > 0 {
+            ip += ( Int( y ) ?? ( registers[y] ?? 0 ) ) - 1
         }
     }
     
+    /// Implements the action for the jnz opcode.
+    ///
+    /// Relative jump by the offset of the value of y if the value of x is not equal to zero.
+    /// - Parameters:
+    ///   - x: The value to test for not equal to zero.
+    ///   - y: The relative jump offset if x != 0.
     func jumpNonZero( x: String, y: String ) -> Void {
-        var xval = 0
-        var yval = 0
-        
-        if let value = Int( x ) {
-            xval = value
-        } else if let value = registers[x] {
-            xval = value
-        }
-        
-        if let value = Int( y ) {
-            yval = value
-        } else if let value = registers[y] {
-            yval = value
-        }
-        
-        if xval != 0 {
-            ip += yval - 1
+        if ( Int( x ) ?? ( registers[x] ?? 0 ) ) != 0 {
+            ip += ( Int( y ) ?? ( registers[y] ?? 0 ) ) - 1
         }
     }
     
+    /// Resets the Coprocessor to its initial state.
     func reset() -> Void {
         registers = [:]
         ip = 0
@@ -222,12 +205,20 @@ public class Coprocessor {
         breakPoints = [:]
     }
     
+    // Returns a string descriptive of the values in the registers.
     func registerValues() -> String {
         let values = [ "a", "b", "c", "d" ].map { "\($0): \(registers[$0]!)" }
         
         return "[" + values.joined( separator: ", " ) + "]"
     }
     
+    /// Runs the Coprocessor until it must stop.
+    /// - The Coprocessor must stop if it is not in the running state.
+    /// - A breakpoint that returns false from its action stops the Coprossor but
+    /// leaves it in the running state.
+    /// - If the ip points outside the memory space then the Coprocessor stops in the halted state.
+    /// - When a rcv opcode is executed and the input queue is empty,
+    ///  the Coprocessor stops in the waiting state.
     public func run() -> Void {
         while state == .running && 0 <= ip && ip < memory.count {
             if let action = breakPoints[ip] {
@@ -239,6 +230,11 @@ public class Coprocessor {
         if state == .running { state = .halted }
     }
     
+    /// Executes a single instruction by the Coprocessor.
+    /// - Does nothing unless the Coprocessor is in the running state
+    /// - Handles ip tracing
+    /// - Handles cycle tracing
+    /// - Updates the ip and cycle count
     func cycle() -> Void {
         guard state == .running else { return }
         
@@ -260,32 +256,55 @@ public class Coprocessor {
         }
     }
     
+    /// Called to allow this Coprocessor to send messages to another.
+    /// - Parameter receiver: The Coprocessor that will receive messages from this one.
     func setReceiver( receiver: Coprocessor ) -> Void {
         self.receiver = receiver
     }
     
+    /// Called when another Coprocessor sends a message to this one.
+    ///
+    /// The message is added to the input queue.  If this Coprocessor is in the waiting state
+    /// it will be switched to the running state.
+    /// - Parameter message: The message from the other Coprocessor.
     func receive( message: Int ) -> Void {
         queue.append( message )
         if state == .waiting { state = .running }
     }
     
+    /// Sets up tracing when the cycle count is within the specified range.
+    /// - Parameters:
+    ///   - start: The cycle count when tracing should start.
+    ///   - stop: The cycle count when tracing should end.
     func setCycleTrace( start: Int, stop: Int ) -> Void {
         cycleTraceStart = start
         cycleTraceStop = stop
     }
     
+    /// Sets up tracing when the ip falls within the specified range.
+    /// - Parameters:
+    ///   - start: The beginning of desired tracing range.
+    ///   - stop: The end of desired tracing range.
     func setIpTrace( start: Int, stop: Int ) -> Void {
         ipTraceStart = start
         ipTraceStop = stop
     }
     
+    /// Adds a breakpoint for the Coprocessor.
+    ///
+    /// The Coprocessor will stop and execute the action **before** executinng the instruction
+    /// at the specified address.
+    /// - Parameters:
+    ///   - address: The address to execute the action before.
+    ///   - action: A closure that returns a Bool - true to keep running, false to stop the Coprocessor.
     public func setBreakPoint( address: Int, action: @escaping ( Coprocessor ) -> Bool ) -> Void {
         breakPoints[address] = action
     }
     
+    /// Returns a disassembly of the Coprocessor device memory.
     func dump() -> Void {
         for ( index, value ) in memory.enumerated() {
-            print( String(format: "%02d:", index), value.mnemonic, value.x, value.y )
+            print( String( format: "%02d:", index ), value.mnemonic, value.x, value.y )
         }
     }
 }
