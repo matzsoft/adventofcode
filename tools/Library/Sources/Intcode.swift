d229e78f70a6107d74fc42178b2be7509cf41c60
Mark Johnson <markj@matzsoft.com>
2023-04-10 20:05:05 -0700

    Library - cleaned up Intcode.swift.
    1. Grouped public properties together.
    2. Added documentation throughout.

--- tools/Library/Sources/Intcode.swift
+++ tools/Library/Sources/Intcode.swift
@@ -7,7 +7,17 @@
 
 import Foundation
 
+/// Implements the Intcode computer used by many problems in 2019.
+///
+/// The instruction set is a little different
+/// - Memory is just an array of integers
+/// - Instruction occupy multiple memory locations
+/// - Instruction length varies by the numer of operands
+/// - There are no registers, data is stored in memory
+///
+/// The emulator provides support for tracing and instruction inspection.
 public class Intcode {
+    /// Breaks down a memory location into an opcode and modes for (potentially) 3 operands.
     public struct Instruction {
         enum Mode: Int { case  position = 0, immediate = 1, relative = 2 }
         public enum Opcode: Int {
@@ -18,7 +28,9 @@ public class Intcode {
         
         public let opcode: Opcode
         let modes: [Mode]
-
+        
+        /// Initializes an Instruction from an integer.
+        /// - Parameter instruction: An integer representing the first word of an instruction.
         init( instruction: Int ) {
             opcode = Opcode( rawValue: instruction % 100 )!
             modes = [
@@ -28,22 +40,31 @@ public class Intcode {
             ]
         }
         
+        /// Returns the mode for one of the operands.
+        /// - Parameter operand: The desired operand, a number from 1 to 3.
+        /// - Returns: The mode of the selected operand.
         func mode( operand: Int ) -> Mode {
             return modes[ operand - 1 ]
         }
     }
 
     let name: String
-    public var ip: Int
     var relativeBase: Int
+    var debug = false
+
+    public var ip: Int
     public var memory: [Int]
     public var inputs: [Int]
-    var debug = false
     
+    /// Returns the next instruction to be executed.
     public var nextInstruction: Instruction {
         return Instruction( instruction: memory[ip] )
     }
-
+    
+    /// Initializes an Intcode computer from an array of integers.
+    /// - Parameters:
+    ///   - name: The name used to identify this particular Intcode computer.
+    ///   - memory: An array of integers to be used as the initial memory of the new Intcode computer.
     public init( name: String, memory: [Int] ) {
         self.name = name
         ip = 0
@@ -52,6 +73,8 @@ public class Intcode {
         self.inputs = []
     }
     
+    /// Initializes an Intcode computer as a copy of another.
+    /// - Parameter other: The other Intcode computer to copy.
     public init( from other: Intcode ) {
         name = other.name
         ip = other.ip
@@ -60,7 +83,19 @@ public class Intcode {
         inputs = other.inputs
         debug = other.debug
     }
-
+    
+    /// Fetches the value of an operand based on the mode.
+    /// - position mode - the operand contains a memory address to fetch the value from.
+    /// - immediate mode - the operand is the desired value.
+    /// - relative mode - the operand contains an offset from the relative base
+    /// for the memory address to fetch the value from.
+    ///
+    /// Attempts to fetch memory from an address less than zero produce an error.
+    /// Attempts to fetch memory from an address beyond the current end of memory return zero.
+    /// - Parameters:
+    ///   - instruction: The instruction used to get the base of the operand.
+    ///   - operand: The operand number.  Either 1, 2, or 3.
+    /// - Returns: The fetched value.
     func fetch( _ instruction: Instruction, operand: Int ) throws -> Int {
         var location = memory[ ip + operand ]
         
@@ -81,6 +116,19 @@ public class Intcode {
         return memory[location]
     }
     
+    /// Store a value into memory at the address determined by the mode of the operand.
+    /// - position mode - The operand contains the memory address to store into.
+    /// - immediate mode - This is not permitted for a store operation and generates an error.
+    /// - relative mode - the operand contains an offset from the relative base
+    /// for the memory address to store the value into.
+    ///
+    /// Attempts to store memory at an address less than zero produce an error.
+    /// Attempts to store memory at an address beyond the current end of memory expands
+    /// the memory with zero values up to that address and then stores the value.
+    /// - Parameters:
+    ///   - instruction: The instruction used to get the base of the operand.
+    ///   - operand: The operand number.  Either 1, 2, or 3.
+    ///   - value: The integer value to be stored.
     func store( _ instruction: Instruction, operand: Int, value: Int ) throws -> Void {
         var location = memory[ ip + operand ]
         
@@ -100,7 +148,11 @@ public class Intcode {
         }
         memory[location] = value
     }
-
+    
+    /// Executes the instruction pointed to by ip.
+    ///
+    /// Additionally ip is updated to point to the next instruction to be executed.
+    /// - Returns: All instructions return nil except out which returns the value to be output.
     public func step() throws -> Int? {
         let instruction = Instruction( instruction: memory[ip] )
         
@@ -161,13 +213,25 @@ public class Intcode {
         return nil
     }
     
+    /// Execute the xxx until the next output or halt instruction.
+    /// - An output instruction will return the value output.
+    /// - A halt instruction will return nil.
     public func execute() throws -> Int? {
         while true {
             if let output = try step() { return output }
             if nextInstruction.opcode == .halt { return nil }
         }
     }
-
+    
+    /// Provides a description (disassembly) of an operand value.
+    ///
+    /// Returns a string with a prefix and the operand value.
+    /// - position mode - the prefix is @
+    /// - immediate mode - no prefix
+    /// - relative mode - the prefix is *
+    /// - Parameters:
+    ///   - instruction: The instruction used to get the mode.
+    ///   - operand: The operand number - either 1, 2, or 3.
     func operandDescription( _ instruction: Instruction, operand: Int ) -> String {
         let location = memory[ ip + operand ]
         
@@ -181,6 +245,14 @@ public class Intcode {
         }
     }
     
+    /// Computes the store location (address) for a given instruction and operand.
+    /// - position mode - the address is the value of the operand.
+    /// - immediate mode - this is invalid and produces an error.
+    /// - relative mode - the address is the value of the operand added to the relative base.
+    /// - Parameters:
+    ///   - instruction: The instruction used to get the mode.
+    ///   - operand: The operand number. Either 1, 2, or 3.
+    /// - Returns: The address that would be stored into.
     func storeLocation( _ instruction: Instruction, operand: Int ) throws -> Int {
         var location = memory[ ip + operand ]
         
@@ -199,7 +271,16 @@ public class Intcode {
         
         return location
     }
-
+    
+    /// Returns a description of the operation of the next instruction to be executed.
+    ///
+    /// The description includes
+    /// - The address of the instruction
+    /// - A disassembly of the instruction
+    /// - The actual values of the operands.
+    /// - The actual values stored and where if any.
+    ///
+    /// Note that the ip is **not** updated.
     public func trace() throws -> String {
         let instruction = Instruction( instruction: memory[ip] )
         var line = String( format: "%04d: ", ip )
