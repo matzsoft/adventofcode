410eca3da74b6c9247725cd498939e7c77632715
Mark Johnson <markj@matzsoft.com>
2023-11-02 14:58:17 -0700

    Revert Library to standard SPM structure.

--- tools/Library/Package.swift
+++ tools/Library/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target( name: "Library", path: "Sources" ),
+        .target(
+            name: "Library"),
         .testTarget(
             name: "LibraryTests",
             dependencies: ["Library"]),
