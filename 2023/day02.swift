2df761df44e13a5f28ef52502fba925162c83271
Mark Johnson <markj@matzsoft.com>
2023-12-01 23:03:27 -0800

    2023 day 2 - streamlined the parsing code.

--- 2023/day02.swift
+++ 2023/day02.swift
@@ -16,34 +16,27 @@ let target = [ 12, 13, 14 ]
 
 struct Game {
     let id: Int
-    let reveals: [[Int]]
-    
+    let reveals: [Int]
     init( line: String ) {
         let sections = line.split { ":;".contains( $0 ) }
         
         id = Int( sections[0].dropFirst( 5 ) )!
-        reveals = sections[1...].map {
-            let words = $0.split { ", ".contains( $0 ) }
+        reveals = sections[1...].reduce( into: [ 0, 0, 0 ] ) { reveals, section in
+            let words = section.split { ", ".contains( $0 ) }
             
-            return stride( from: 0, to: words.count, by: 2 ).reduce( into: [ 0, 0, 0 ] ) { reveal, index in
-                for index in stride( from: 0, to: words.count, by: 2 ) {
-                    let color = colors.firstIndex( of: String( words[index+1] ) )!
-                    reveal[color] = Int( words[index] )!
-                }
+            stride( from: 0, to: words.count, by: 2 ).forEach { index in
+                let color = colors.firstIndex( of: String( words[index+1] ) )!
+                reveals[color] = max( reveals[color], Int( words[index] )! )
             }
         }
     }
     
     func isPossible( target: [Int] ) -> Bool {
-        reveals.first { reveal in reveal.indices.first { reveal[$0] > target[$0] } != nil } == nil
+        reveals.indices.first { reveals[$0] > target[$0] } == nil
     }
     
     var power: Int {
-        reveals
-            .reduce( into: [ 0, 0, 0 ] ) { maximums, reveal in
-                reveal.indices.forEach { maximums[$0] = max( reveal[$0], maximums[$0] ) }
-            }
-            .reduce( 1, * )
+        reveals.reduce( 1, * )
     }
 }
 
