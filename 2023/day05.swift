89e84689af51de285921ccbf3805f7fe1f95e0b8
Mark Johnson <markj@matzsoft.com>
2023-12-05 22:47:30 -0800

    2023 day 5 - 1st optimization.
    Reduced part 2 runtime from 566 seconds to 177 seconds by combining the
    maps into a single map that goes directly from seed to location.

--- 2023/day05.swift
+++ 2023/day05.swift
@@ -11,14 +11,59 @@
 import Foundation
 import Library
 
+extension Range<Int> {
+    func offset( by offset: Int ) -> Range<Int> {
+        ( lowerBound + offset ) ..< ( upperBound + offset )
+    }
+    
+    func subtracting( other: Range<Int> ) -> Range<Int> {
+        let clamped = clamped( to: other )
+        return clamped.upperBound ..< upperBound
+    }
+}
+
 struct SourceRange {
     let range: Range<Int>
     let addend: Int
+    
+    var lowerBound: Int { range.lowerBound }
+    var upperBound: Int { range.upperBound }
+    
+    init( _ range: Range<Int>, addend: Int ) {
+        self.range = range
+        self.addend = addend
+    }
     
     init( line: String ) {
         let numbers = line.split( separator: " " ).map { Int( $0 )! }
         range = numbers[1] ..< ( numbers[1] + numbers[2] )
         addend = numbers[0] - numbers[1]
+    }
+    
+    func clamped( to other: Range<Int> ) -> SourceRange {
+        SourceRange( range.clamped( to: other ), addend: addend )
+    }
+    
+    func offset( by offset: Int ) -> SourceRange {
+        SourceRange( range.offset( by: offset ), addend: addend )
+    }
+    
+    func blend( with others: [SourceRange] ) -> [SourceRange] {
+        var last = lowerBound
+        var newRanges = [SourceRange]()
+        
+        for other in others {
+            if last < other.lowerBound {
+                newRanges.append( SourceRange( last ..< other.lowerBound, addend: addend ) )
+            }
+            newRanges.append( SourceRange( other.range, addend: addend + other.addend ) )
+            last = other.upperBound
+        }
+        if last < upperBound {
+            newRanges.append( SourceRange( last ..< upperBound, addend: addend ) )
+        }
+        
+        return newRanges
     }
 }
 
@@ -27,6 +72,12 @@ struct Map {
     let destination: String
     let ranges: [SourceRange]
     
+    init( source: String, destination: String, ranges: [SourceRange] ) {
+        self.source = source
+        self.destination = destination
+        self.ranges = ranges
+    }
+    
     init( lines: [String] ) {
         let header = lines[0].split( whereSeparator: { "- ".contains( $0 ) } )
         source = String( header[0] )
@@ -34,6 +85,37 @@ struct Map {
         ranges = lines[1...]
             .map { SourceRange( line: $0 ) }
             .sorted { $0.range.lowerBound < $1.range.lowerBound }
+    }
+    
+    func merge( other: Map ) -> Map {
+        var last = Int.min
+        var newRanges = [SourceRange]()
+        
+        func handleUnaffected( clampRange: Range<Int> ) {
+            let otherRanges = other.ranges
+                .map { $0.clamped( to: clampRange ) }
+                .filter { !$0.range.isEmpty }
+            
+            newRanges.append( contentsOf: otherRanges )
+        }
+        
+        for range in ranges {
+            if last < range.lowerBound {
+                handleUnaffected( clampRange: last ..< range.lowerBound )
+            }
+            
+            let otherRanges = other.ranges
+                .map { $0.clamped( to: range.range.offset( by: range.addend ) ) }
+                .filter { !$0.range.isEmpty }
+                .map { $0.offset( by: -range.addend ) }
+            
+            newRanges.append( contentsOf: range.blend( with: otherRanges ) )
+            last = range.upperBound
+        }
+        
+        handleUnaffected( clampRange: last ..< Int.max )
+
+        return Map( source: source, destination: other.destination, ranges: newRanges )
     }
 }
 
@@ -65,19 +147,24 @@ func locationFor( seed: Int, maps: [ String: Map ] ) -> Int {
 
 func part1( input: AOCinput ) -> String {
     let ( seeds, maps ) = parse( input: input )
-    let locations = seeds.reduce( into: [ Int : Int ]() ) { $0[$1] = locationFor( seed: $1, maps: maps ) }
     
-    return "\( locations.min( by: { $0.value < $1.value } )!.value )"
+    return "\( seeds.map { locationFor( seed: $0, maps: maps ) }.min()! )"
 }
 
 
 func part2( input: AOCinput ) -> String {
     let ( seeds, maps ) = parse( input: input )
+    var baseMap = maps["seed"]!
+    
+    while baseMap.destination != "location" {
+        baseMap = baseMap.merge( other: maps[ baseMap.destination]! )
+    }
+    let mergedMaps = [ "seed" : baseMap ]
     var minLocation = Int.max
     
-    for index in stride(from: 0, to: seeds.count, by: 2 ) {
+    for index in stride( from: 0, to: seeds.count, by: 2 ) {
         for seed in seeds[index] ..< ( seeds[index] + seeds[index+1] ) {
-            minLocation = min( minLocation, locationFor( seed: seed, maps: maps ) )
+            minLocation = min( minLocation, locationFor( seed: seed, maps: mergedMaps ) )
         }
     }
     
