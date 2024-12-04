1d823aec0adda6f78ce079efdad5a24c828ad740
Mark Johnson <markj@matzsoft.com>
2024-12-03 17:32:56 -0800

    2024 day 3 - converted to regex.
    Much clearer but much slower also.

--- 2024/day03.swift
+++ 2024/day03.swift
@@ -11,104 +11,36 @@
 import Foundation
 import Library
 
-
-func scan( line: String, conditionals: Bool ) -> [Int] {
-    var state = 0
-    var left = ""
-    var right = ""
+func process( matches: [Regex<Substring>.Match] ) -> Int {
     var enabled = true
-    var result: [Int] = []
-    
-    for character in line {
-        if character == "m" { state = 1; continue }
-        if character == "d" && conditionals { state = 9; continue }
-        
-        switch state {
-        case 0:
-            break
-        case 1:
-            left = ""
-            right = ""
-            if character == "u" { state = 2 } else { state = 0 }
-        case 2:
-            if character == "l" { state = 3 } else { state = 0 }
-        case 3:
-            if character == "(" { state = 4 } else { state = 0 }
-        case 4:
-            if character.isNumber {
-                left.append( String( character ) )
-                state = 5
-            } else {
-                state = 0
-            }
-        case 5:
-            if character.isNumber {
-                left.append( String( character ) )
-            } else if character == "," {
-                state = 6
-            } else {
-                state = 0
-            }
-        case 6:
-            if character.isNumber {
-                right.append( String( character ) )
-                state = 7
-            } else {
-                state = 0
-            }
-        case 7:
-            if character.isNumber {
-                right.append( String( character ) )
-            } else if character == ")" {
-                if enabled {
-                    result.append( Int( left )! * Int( right )! )
-                }
-                state = 0
-            } else {
-                state = 0
-            }
+    let regex = #/mul\((\d+),(\d+)\)/#
 
-        case 9:
-            if character == "o" { state = 10 } else { state = 0 }
-       case 10:
-            switch character {
-            case "(":
-                state = 11
-            case "n":
-                state = 12
-            default:
-                state = 0
+    return matches.reduce( 0 ) { sum, match -> Int in
+        if match.0.hasPrefix( "mul" ) && enabled {
+            if let first = try! regex.firstMatch( in: match.0 ) {
+                return sum + Int( first.1 )! * Int( first.2 )!
             }
-        case 11:
-            if character == ")" { enabled = true }
-            state = 0
-        case 12:
-            if character == "'" { state = 13 } else { state = 0 }
-        case 13:
-            if character == "t" { state = 14 } else { state = 0 }
-        case 14:
-            if character == "(" { state = 15 } else { state = 0 }
-        case 15:
-            if character == ")" { enabled = false }
-            state = 0
-        default:
-            fatalError( "Error in FSA" )
+        } else if match.0 == "do()" {
+            enabled = true
+        } else if match.0 == "don't()" {
+            enabled = false
         }
+        return sum
     }
-    
-    return result
 }
 
 
 func part1( input: AOCinput ) -> String {
-    let pairs = scan( line: input.lines.joined(), conditionals: false )
-    return "\( pairs.reduce( 0, + ) )"
+    let regex = #/mul\(\d+,\d+\)/#
+    let matches = input.lines.joined().matches( of: regex )
+    return "\(process( matches: matches ))"
 }
 
 
 func part2( input: AOCinput ) -> String {
-    let pairs = scan( line: input.lines.joined(), conditionals: true )
-    return "\( pairs.reduce( 0, + ) )"
+    let regex = #/mul\(\d+,\d+\)|do\(\)|don't\(\)/#
+    let matches = input.lines.joined().matches( of: regex )
+    return "\(process( matches: matches ))"
 }
 
 
