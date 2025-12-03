4d7fc4e732f55091a879234d657c79a126040a60
Mark Johnson <markj@matzsoft.com>
2025-12-02 22:50:16 -0800

    First working version.

---
+++ 2025/day03.swift
@@ -0,0 +1,79 @@
+//
+//         FILE: day03.swift
+//  DESCRIPTION: Advent of Code 2025 Day 3: Lobby
+//        NOTES: ---
+//       AUTHOR: Mark T. Johnson, markj@matzsoft.com
+//    COPYRIGHT: © 2025 MATZ Software & Consulting. All rights reserved.
+//      VERSION: 1.0
+//      CREATED: 12/02/25 21:00:01
+//
+
+import Foundation
+import Library
+
+
+func joltage( bank: [Int] ) -> Int {
+    var jolts = 0
+    for i in 0 ..< bank.count - 1 {
+        for j in i + 1 ..< bank.count {
+            jolts = max( jolts, 10 * bank[i] + bank[j] )
+        }
+    }
+    return jolts
+}
+
+
+func bestJolts( bank: [Int], size: Int ) -> [Int] {
+    guard size > 0 else { return [] }
+    
+    let big = bank.max()!
+    let bigIndex = bank.firstIndex( of: big )!
+    
+    if bank.count - bigIndex >= size {
+        let next = bank[(bigIndex + 1)...]
+        return [big] + bestJolts( bank: Array( next ), size: size - 1 )
+    }
+    
+    for next in stride( from: big - 1, to: 0, by: -1 ) {
+        if let index = bank[..<bigIndex].firstIndex( of: next ) {
+            if bank.count - index >= size {
+                let nextBank = bank[(index + 1)...]
+                return [next] + bestJolts( bank: Array( nextBank ), size: size - 1 )
+            }
+        }
+    }
+    fatalError( "Argh!" )
+}
+
+
+func maxJoltage( bank: [Int] ) -> Int {
+    let joltages = bestJolts( bank: bank, size: 12 )
+    let biggest = joltages.map { String( $0 ) }.joined()
+    return Int( biggest )!
+}
+
+
+func parse( input: AOCinput ) -> [[Int]] {
+    return input.lines.map { Array( $0 ).map { Int( String( $0 ) )! } }
+}
+
+
+func part1( input: AOCinput ) -> String {
+    let banks = parse( input: input )
+    let totalJolts = banks.map { joltage( bank: $0 ) }.reduce( 0, + )
+    return "\(totalJolts)"
+}
+
+
+func part2( input: AOCinput ) -> String {
+    let banks = parse( input: input )
+    let totalJolts = banks.map { maxJoltage( bank: $0 ) }.reduce( 0, + )
+    return "\(totalJolts)"
+}
+
+
+try print( projectInfo() )
+try runTests( part1: part1 )
+try runTests( part2: part2 )
+try solve( part1: part1 )
+try solve( part2: part2 )
