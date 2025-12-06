250729876ab4e3c685eac80e6f1826432676b243
Mark Johnson <markj@matzsoft.com>
2025-12-06 12:36:37 -0800

    2025 day 6 - did some minor simplifications.
    1. Moved complex code from Problem.init to part2.
    2. This allowed the removal of both init-s from Problem.
    3. Removed the function allIndices and put the code inline.

--- 2025/day06.swift
+++ 2025/day06.swift
@@ -23,24 +23,6 @@ struct Problem {
         case .multipy: return numbers.reduce( 1, * )
         }
     }
-    
-    init( operation: Problem.Operator, numbers: [Int] ) {
-        self.operation = operation
-        self.numbers = numbers
-    }
-    
-    init( operations: [Operator], rows: [[[Character]]], index: Int ) {
-        let elements = rows.indices.map { rows[ $0 ][ index ] }
-        let numbers = elements[0].indices.reduce( into: [Int]() ) { numbers, i in
-            let number = rows.indices.reduce( into: [Character]() ) { number, j in
-                number.append( elements[ j ][ i ] )
-            }.filter { $0 != " " }
-            numbers.append( Int( String( number ) )! )
-        }
-        
-        operation = operations[ index ]
-        self.numbers = numbers
-    }
 }
 
 
@@ -61,20 +43,19 @@ func part1( input: AOCinput ) -> String {
 }
 
 
-func allIndices( row: [Character] ) -> [Int] {
-    row.indices.filter { Problem.Operator( rawValue: String( row[ $0 ] ) ) != nil }
-}
-
-
 func part2( input: AOCinput ) -> String {
     let operationRow = Array( input.lines.last! )
     let operations = operationRow.compactMap { Problem.Operator( rawValue: String( $0 ) ) }
-    let columnIndices = allIndices( row: operationRow )
+    let columnIndices = operationRow.indices.filter {
+        Problem.Operator( rawValue: String( operationRow[ $0 ] ) ) != nil
+    }
     let rows = input.lines.dropLast().map {
         let row = Array( $0 )
         let chunks = columnIndices.indices.reduce( into: [[Character]]() ) { chunks, index in
             if index < columnIndices.count - 1 {
-                chunks.append( Array( row[ columnIndices[ index ] ..< columnIndices[ index + 1 ] - 1 ] ) )
+                chunks.append(
+                    Array( row[ columnIndices[ index ] ..< columnIndices[ index + 1 ] - 1 ] )
+                )
             } else {
                 chunks.append( Array( row[columnIndices[ index ]...] ) )
             }
@@ -82,8 +63,16 @@ func part2( input: AOCinput ) -> String {
         return chunks
     }
     
-    let problems = operations.indices.map {
-        Problem( operations: operations, rows: rows, index: $0 )
+    let problems = operations.indices.map { index in
+        let elements = rows.indices.map { rows[ $0 ][ index ] }
+        let numbers = elements[0].indices.reduce( into: [Int]() ) { numbers, i in
+            let number = rows.indices.reduce( into: [Character]() ) { number, j in
+                number.append( elements[ j ][ i ] )
+            }.filter { $0 != " " }
+            numbers.append( Int( String( number ) )! )
+        }
+        
+        return Problem( operation: operations[ index ], numbers: numbers )
     }
     
     return "\(problems.reduce( 0 ) { $0 + $1.value } )"
